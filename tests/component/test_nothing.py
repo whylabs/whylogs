b3e1a3dc23cbf1aaa19b24ddd3296f25b85d746b
Isaac Backus <isaac@whylabs.ai>
2020-07-17 10:56:38 -0700

    Test files for pycharm test discovery

---
+++ tests/component/test_nothing.py
@@ -0,0 +1,9 @@
+"""
+Empty test file required to allow PyCharm to discover tests in this directory.
+
+Without this file, there is no good way to get PyCharm to run tests just in
+this directory
+
+It appears PyCharm's test folder discovery needs a test_xxx.py file in that
+directory.
+"""
