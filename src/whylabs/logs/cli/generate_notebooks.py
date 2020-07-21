d3bb2c425f6d2f8d6d2c9c09865c4dc835bbffb7
Bernease Herman <bernease@gmail.com>
2020-07-21 04:08:00 -0700

    Add docstrings and minor formatting

--- src/whylabs/logs/cli/generate_notebooks.py
+++ src/whylabs/logs/cli/generate_notebooks.py
@@ -4,13 +4,27 @@ import shutil
 
 CONFIG = {}
 
+
 def substitute(matchobj):
+    """Returns string to replace for given match object (re package)."""
     if matchobj.group(1) in CONFIG.keys():
         return CONFIG[matchobj.group(1)]
     else:
         return "None"
 
+
 def generate_notebooks(project_dir, config_dict={}):
+    """
+    Copies notebooks from project-level notebooks directory to given project
+    directory, replacing specially-marked comments with values from config.
+cl
+    Parameters
+    ----------
+    project_dir: string
+        Path-like string pointing to project directory
+    config_dict: dict
+        Additional configuration variables
+    """
     # make project notebook dir
     project_nb_path = os.path.join(project_dir, "notebooks")
     if not os.path.isdir(project_nb_path):
@@ -24,11 +38,8 @@ def generate_notebooks(project_dir, config_dict={}):
     assert(os.path.isfile(os.path.join(package_nb_path, "Analysis.ipynb")))
 
     # incorporate config parameter
-    # TODO: may need to change to file read of config upon cli complete
     CONFIG["PROJECT_DIR"] = project_dir
     CONFIG.update(config_dict)
-
-    print(CONFIG)
 
     # copy over static Logging.ipynb to project nb dir
     shutil.copyfile(os.path.join(package_nb_path, "Logging.ipynb"),
@@ -40,12 +51,13 @@ def generate_notebooks(project_dir, config_dict={}):
     if os.path.isfile(project_analysis_nb_path):
         os.remove(project_analysis_nb_path)
 
-    with open(os.path.join(package_nb_path, "Analysis.ipynb"), "r") as orig_nb:
+    with open(os.path.join(package_nb_path, "Analysis.ipynb"), 'r') as orig_nb:
         orig_analysis_lines = orig_nb.readlines()
 
-    with open(project_analysis_nb_path, "w") as out_nb:
+    with open(project_analysis_nb_path, 'w') as out_nb:
         for line in orig_analysis_lines:
             out_nb.write(re.sub("###(.*)###", substitute, line))
+
 
 if __name__ == "__main__":
     generate_notebooks("/Users/bernease/testproj",
