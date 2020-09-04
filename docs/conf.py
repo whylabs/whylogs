75966ff9a0b8846ddcf4e47c943bc5481481a519
Isaac Backus <isaac@whylabs.ai>
2020-09-04 09:37:30 -0400

    Infer whylogs version by parsing _version.py to fix a bug with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ from sphinx.ext.autodoc import between
 # Add package paths to the python path to allow sphinx to import modules
 sys.path.insert(0, os.path.abspath("../src/"))
 
-import whylogs
+
 
 # -- Project information -----------------------------------------------------
 
@@ -34,7 +34,27 @@ project = "WhyLogs"
 copyright = "2020, WhyLabs, Inc"
 author = "WhyLabs"
 gitstamp_fmt = "%d %b %Y"
-version = whylogs.__version__
+
+
+def read(rel_path):
+    import codecs
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delimiter = '"' if '"' in line else "'"
+            v = line.split(delimiter)[1]
+            print(f"WhyLogs version: {v}")
+            return v
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+version = get_version("../src/whylogs/_version.py")
+
 
 # -- General configuration ---------------------------------------------------
 
