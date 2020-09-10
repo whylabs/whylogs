9c004edf39554e983cca75d6919f6e9ce1f89dfe
Andy Dang🤖 <andy@whylabs.ai>
2020-09-10 14:54:20 -0700

    Fix code styling

--- examples/configure_logger.py
+++ examples/configure_logger.py
@@ -9,17 +9,17 @@ from whylogs.app.session import get_or_create_session
 import pandas as pd
 
 # Load some example data, using 'issue_d' as a datetime column
-df = pd.read_csv('data/lending_club_1000.csv', parse_dates=['issue_d'])
+df = pd.read_csv("data/lending_club_1000.csv", parse_dates=["issue_d"])
 
 # Create a WhyLogs logging session
 session = get_or_create_session()
 # Log statistics for the dataset with config options
 with session.logger(
-        dataset_name='lending-club', dataset_timestamp=df['issue_d'].max(),
-    ) as ylog:
+    dataset_name="lending-club", dataset_timestamp=df["issue_d"].max(),
+) as ylog:
     ylog.log_dataframe(df)
     # Note that the logger is active within this context
-    print('Logger is active:', ylog.is_active())
+    print("Logger is active:", ylog.is_active())
 
 # The logger is no longer active
-print('Logger is active:', ylog.is_active())
+print("Logger is active:", ylog.is_active())
--- examples/log_dataframe.py
+++ examples/log_dataframe.py
@@ -15,7 +15,7 @@ from whylogs.app.session import get_or_create_session
 import pandas as pd
 
 # Load some example data
-df = pd.read_csv('data/lending_club_1000.csv')
+df = pd.read_csv("data/lending_club_1000.csv")
 
 # Create a WhyLogs logging session
 session = get_or_create_session()
