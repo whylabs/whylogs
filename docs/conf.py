a3a2b596d13728079e0b68d5bd948e331258f46c
Andy Dang🤖 <andy@whylabs.ai>
2020-09-22 16:51:26 -0700

    Update ReadMe to link to example repo and notebooks
    Bump version: 0.0.2b22 → 0.0.2
    
    Bump version: 0.0.2 → 0.1.0b0

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ def read(rel_path):
 
 
 # This string is managed by bump2version
-version = " 0.0.2b22"
+version = " 0.1.0b0"
 
 
 # -- General configuration ---------------------------------------------------
