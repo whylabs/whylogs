f0be96fbd77fb4d41bd2dc773bb796597f5472c5
Bernease Herman <bernease@gmail.com>
2020-09-21 19:48:50 -0700

    Formatting changes to please the code formatter

--- src/whylogs/viz/visualizer.py
+++ src/whylogs/viz/visualizer.py
@@ -5,24 +5,27 @@ from .base import BaseProfileVisualizer
 
 class ProfileVisualizer(BaseProfileVisualizer):
     """"""
-    def __init__(self, framework='matplotlib'):
+
+    def __init__(self, framework="matplotlib"):
         """Initializes the ProfileVisualizer class."""
-        self.available_frameworks = ['matplotlib']
+        self.available_frameworks = ["matplotlib"]
         self.framework = framework
         self.visualizer = self.__subclass_framework(framework)
         super().__init__(framework=self.framework, visualizer=self.visualizer)
 
-
-    def __subclass_framework(self, framework='matplotlib'):
-        if framework=='matplotlib':
+    def __subclass_framework(self, framework="matplotlib"):
+        if framework == "matplotlib":
             from .matplotlib.visualizer import MatplotlibProfileVisualizer
+
             return MatplotlibProfileVisualizer()
 
-        warn(f"""Unable to use chosen visualization framework. Please choose
+        warn(
+            f"""Unable to use chosen visualization framework. Please choose
                 one of the following frameworks: {self.available_frameworks}
-                using visualizer.enable_framework().""", RuntimeWarning)
+                using visualizer.enable_framework().""",
+            RuntimeWarning,
+        )
         return
 
-
-    def enable_framework(self, framework='matplotlib'):
+    def enable_framework(self, framework="matplotlib"):
         return self.__subclass_framework(framework)
