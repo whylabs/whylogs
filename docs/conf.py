5fa66f296c90b598e5cc0bcb1cf00a33678b510c
Andy Dang🤖 <andy@whylabs.ai>
2020-10-22 16:58:55 -0700

    Bump version: 0.1.4b0 → 0.1.4

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ def read(rel_path):
 
 
 # This string is managed by bump2version
-version = " 0.1.4b0"
+version = " 0.1.4"
 
 
 # -- General configuration ---------------------------------------------------
