194e14102d4a2fa9ba6e29b5eaae2441ae77f37a
Andy Dang🤖 <andy@whylabs.ai>
2020-10-22 16:58:50 -0700

    Bump version: 0.1.3 → 0.1.4b0

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ def read(rel_path):
 
 
 # This string is managed by bump2version
-version = " 0.1.3"
+version = " 0.1.4b0"
 
 
 # -- General configuration ---------------------------------------------------
--- src/whylogs/_version.py
+++ src/whylogs/_version.py
@@ -1,3 +1,3 @@
 """WhyLabs version number."""
 
-__version__ = "0.1.3"
+__version__ = "0.1.4b0"
