6e10b2e7cc790fac0654e9dcd3d0fed3962afeb3
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2020-12-04 11:38:27 -0800

    Bump version: 0.1.8 → 0.1.9-dev0

--- docs/conf.py
+++ docs/conf.py
@@ -108,7 +108,7 @@ copyright = "2020, WhyLabs, Inc"
 # built documents.
 #
 # The short X.Y version.
-version = " 0.1.8"
+version = " 0.1.9-dev0"
 # The full version, including alpha/beta/rc tags.
 release = ""  # Is set by calling `setup.py docs`
 
--- src/whylogs/_version.py
+++ src/whylogs/_version.py
@@ -1,3 +1,3 @@
 """WhyLabs version number."""
 
-__version__ = "0.1.8"
+__version__ = "0.1.9-dev0"
