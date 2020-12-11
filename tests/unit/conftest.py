b596da801a4c230892abec33f608dfd0c6d905fa
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2020-12-11 15:04:55 -0800

    missing conftest file

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,10 +1,16 @@
 import os
 import sys
-
+import pytest
 _MY_DIR = os.path.realpath(os.path.dirname(__file__))
 # Allow import of the test utilities packages
 sys.path.insert(0, os.path.join(_MY_DIR, os.pardir, "helpers"))
 # Test the parent package
-sys.path.insert(0, os.path.join(_MY_DIR, os.pardir))
+sys.path.insert(0, os.path.join(_MY_DIR, os.pardir,os.pardir,"testdata"))
 # Verify whylogs is importable
 import whylogs
+
+@pytest.fixture(scope="session")
+def df_lending_club():
+    import pandas as pd
+    df = pd.read_csv(os.path.join(_MY_DIR, os.pardir,os.pardir,"testdata", "lending_club_1000.csv"))
+    return df
