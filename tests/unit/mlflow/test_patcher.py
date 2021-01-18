4d002adb60fbaf6da2d9d2dbb28e8e5ddca76ccf
Andy Dang🤖 <andy@whylabs.ai>
2021-01-18 09:28:23 -0800

    Add a test with logging a model and verify that we hijacts Model.log method

--- tests/unit/mlflow/test_patcher.py
+++ tests/unit/mlflow/test_patcher.py
@@ -81,3 +81,21 @@ def test_assert_log_artifact_is_called_twice(tmpdir):
 
         assert log_artifact.call_count == 2
     whylogs.mlflow.disable_mlflow()
+
+
+def test_sklearn_model_log(tmpdir):
+    import whylogs
+    import mlflow
+    from sklearn.linear_model import ElasticNet
+    from whylogs.mlflow import patcher as whylogs_patcher
+
+    set_up_mlflow(mlflow, tmpdir)
+    with mock.patch.object(whylogs_patcher, "new_model_log") as new_log_func:
+        whylogs.enable_mlflow()
+
+        with mlflow.start_run():
+            model = ElasticNet(alpha=0.5, l1_ratio=0.5, random_state=42)
+            mlflow.sklearn.log_model(model, "model", registered_model_name="TestModel")
+
+        assert new_log_func.call_count == 1
+    whylogs.mlflow.disable_mlflow()
