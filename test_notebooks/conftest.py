d30ccf610267792ef8f94cb682604a8b7007511d
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2021-02-04 19:36:55 -0800

    add test for notebooks

---
+++ test_notebooks/conftest.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import pytest
+_MY_DIR = os.path.realpath(os.path.dirname(__file__))
+# Allow import of the test utilities packages
+
+
+@pytest.fixture(scope="session")
+def notebooks_path():
+
+    notebook_path = os.path.join(
+        _MY_DIR, os.pardir, os.pardir, "notebooks")
+    return os.path.abs(notebook_path)
