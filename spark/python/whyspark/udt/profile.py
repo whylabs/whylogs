5547768e2f1bebeacd38ced16f75b0e35eee6e6f
Andy Dang🤖 <andy@whylabs.ai>
2021-02-07 11:03:11 -0800

    Add helper method to write data to parquet format

--- spark/python/whyspark/udt/profile.py
+++ spark/python/whyspark/udt/profile.py
@@ -8,6 +8,7 @@ class WhyProfileSession:
     """
     A class that enable easy access to the profiling API
     """
+
     def __init__(self, dataframe: DataFrame, name: str, time_column: Optional[str] = None, group_by_columns=None):
         if group_by_columns is None:
             group_by_columns = []
@@ -27,14 +28,14 @@ class WhyProfileSession:
         :rtype: WhyLogSession
         """
         return WhyProfileSession(dataframe=self._df, name=self._name, time_column=time_column,
-                             group_by_columns=self._group_by_columns)
+                                 group_by_columns=self._group_by_columns)
 
     def groupBy(self, col: str, *cols):  # noqa
         return WhyProfileSession(dataframe=self._df, name=self._name, time_column=self._time_colunn,
-                             group_by_columns=[col] + list(cols))
+                                 group_by_columns=[col] + list(cols))
 
     def aggProfiles(self, datetime_ts: Optional[datetime] = None, timestamp_ms: int = None) -> DataFrame:  # noqa
-        jvm = self._df.sql_ctx._sc._jvm # noqa
+        jvm = self._df.sql_ctx._sc._jvm  # noqa
         j_session = jvm.com.whylogs.spark.WhyLogs.newProfilingSession(self._df._jdf, self._name)  # noqa
 
         if self._time_colunn is not None:
@@ -49,6 +50,16 @@ class WhyProfileSession:
             timestamp_ms = int(datetime.now(tz=timezone.utc).timestamp() * 1000)
 
         return j_session.aggProfiles(timestamp_ms)
+
+    def aggParquet(self, path: str, datetime_ts: Optional[datetime] = None, timestamp_ms: int = None):  # noqa
+        """
+        A helper method to aggregate data and write to a parquet path
+        :param path: the Parquet path. In a file system that Spark supports
+        :param datetime_ts: Optional. The session timestamp as a datetime object
+        :param timestamp_ms: Optional. The session timestamp in milliseconds
+        """
+        df = self.aggProfiles(datetime_ts=datetime_ts, timestamp_ms=timestamp_ms)
+        df.write.parquet(path)
 
 
 def new_profiling_session(df: DataFrame, name: str, time_column: Optional[str] = None):
