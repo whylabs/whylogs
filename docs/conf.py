4c67b527216dd8cb6f2e1f0453b91e123de7087b
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2021-02-22 13:36:09 -0800

    Bump version: 0.3.2-dev0 → 0.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -119,7 +119,7 @@ copyright = "2020, WhyLabs, Inc"
 # built documents.
 #
 # The short X.Y version.
-version = "0.3.2-dev0"
+version = "0.3.2"
 # The full version, including alpha/beta/rc tags.
 release = ""  # Is set by calling `setup.py docs`
 
