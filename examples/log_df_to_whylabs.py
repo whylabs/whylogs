af7ac06e3358d0bfe5d2239a4f1ee1b8b3b37cf8
Alex <alex@whylabs.ai>
2021-03-16 15:34:33 -0700

    update comment

--- examples/log_df_to_whylabs.py
+++ examples/log_df_to_whylabs.py
@@ -2,7 +2,7 @@
 Log a dataframe and send the profile to WhyLabs for visualization
 ===============
 
-Example for logging a dataframe and sending the results to WhyLabs, where the profile can be explored further
+Example for logging a dataframe and sending the results to WhyLabs, where the data can be explored further
 """
 import pandas as pd
 from whylogs.app.session import start_whylabs_session
@@ -10,11 +10,11 @@ from whylogs.app.session import start_whylabs_session
 # Load some sample data
 df = pd.read_csv("data/lending_club_1000.csv")
 
-# Create a special whylabs logging session
+# Create a WhyLabs logging session
 # Note: data collection consent must be explicitly provided
 with start_whylabs_session(data_collection_consent=True) as session:
     # Log statistics for the dataset
-    # Resulting dataset profile will be sent to WhyLabs,
-    # and you will receive a link to view it!
+    # Resulting dataset profile(s) will be sent to WhyLabs,
+    # and you will receive a link to view the pretty charts!
     with session.logger() as ylog:
         ylog.log_dataframe(df)
