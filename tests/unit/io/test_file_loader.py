8d38e8c4983d6bbbd92ca8d8cf6f7bcc7b3ac4ae
Anthony Naddeo <anthony.naddeo@gmail.com>
2021-04-15 15:47:49 -0700

    Fix excel test for python 3.6
    Pandas doesn't work with excel in our version unfortunately.

--- tests/unit/io/test_file_loader.py
+++ tests/unit/io/test_file_loader.py
@@ -19,9 +19,12 @@ def test_image_loader(image_files):
 
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python3.7 or higher")
 def test_file_loader(file_list):
+    filefmts = [(pd.DataFrame, "csv"), (list, "jsonl")]
 
-    filefmts = [(pd.DataFrame, "excel"),
-                (pd.DataFrame, "csv"), (list,  "jsonl")]
+    # Our version of pandas doesn't support excel. If we upgrade to
+    # pandas >= 1.2 then we can stop conditionally adding excel.
+    if "3.6" not in sys.version:
+        filefmts.append((pd.DataFrame, "excel"))
 
     for idx, file_path in enumerate(file_list):
         (data, magic_data), filefmt = file_loader(file_path)
