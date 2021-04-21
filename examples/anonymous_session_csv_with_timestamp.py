e8da93a9158a39f367190e7df2e4790304fb5846
Anthony Naddeo <anthony.naddeo@gmail.com>
2021-04-20 21:30:13 -0700

    Add tqdm for progress in sessions
    This adds much needed progress bars for the guest session feature.

--- examples/anonymous_session_csv_with_timestamp.py
+++ examples/anonymous_session_csv_with_timestamp.py
@@ -8,27 +8,29 @@ the dataset up by each day using the Calendar_Week column and log each of the da
 from datetime import datetime
 
 import pandas as pd
+from tqdm import tqdm
 
-from whylogs.app.session import LoggerKey, start_whylabs_session
+from whylogs.app.session import start_whylabs_session
 
 csv_file = "data/sample_media_spend.csv"
 
 # Load some sample data
 print(f"Loading {csv_file}")
 csv_dataframe = pd.read_csv(csv_file)
+grouped_data = csv_dataframe.groupby(["Calendar_Week"])
 
 # Create a WhyLabs logging session
 # Note: data collection consent must be explicitly provided since we'll be uploading profiles to WhyLabs.
-with start_whylabs_session(data_collection_consent=True) as session:
+with start_whylabs_session(data_collection_consent=True) as session, tqdm(grouped_data) as t:
     # Group each of the rows by the day they occur on using the date string in the Calendar_Week col
-    for day_string, dataframe_for_day in csv_dataframe.groupby(["Calendar_Week"]):
+    for day_string, dataframe_for_day in t:
         # This dataset has dates of the form 9/5/2020
         dt = datetime.strptime(day_string, "%m/%d/%Y")
-        print(f"Logging data for {day_string}")
+        t.set_description(f"Logging data for {day_string}")
 
         # whylabs loggers are specific to the dataset's timestamp so we'll be using a different one for each
         # date in our dataset.
-        logger = session.logger(args=LoggerKey(dataset_timestamp=dt))
+        logger = session.logger(dataset_timestamp=dt)
 
         # log the data to the logger. The logger will write this data out in binary form when it closes, which
         # at the end of the with block in the session's internal logic.
