1a8e6fc9151476b6e58b6c5164f73ece77ed0efd
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2021-04-28 09:37:05 -0700

    relese 0.4.5 versioning

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,7 @@ copyright = "2020, WhyLabs, Inc"
 # built documents.
 #
 # The short X.Y version.
-version = "0.4.5-dev1"
+version = "0.4.5"
 # The full version, including alpha/beta/rc tags.
 release = ""  # Is set by calling `setup.py docs`
 
