e43274ce49248013271465f59cb488f494a76633
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2021-05-03 15:29:24 -0700

    format and lint fixes

--- examples/rov_whylogs/predict_logger_longrun.py
+++ examples/rov_whylogs/predict_logger_longrun.py
@@ -1,11 +1,12 @@
-import json
 from collections import deque
-from logger_tools import log_session
-from kafkaConnector import create_consumer
-import numpy as np
 
-topic = 'prediction-rov'
+import numpy as np
+from kafkaConnector import create_consumer
+from logger_tools import log_session
+
+topic = "prediction-rov"
 consumer = create_consumer(topic)
+
 
 def main():
     resid_window = deque(maxlen=15)
@@ -17,58 +18,60 @@ def main():
     total = 0
 
     while True:
-        record = consumer.poll(timeout_ms=session_timeout*1000, max_records=100, update_offsets=True)
+        record = consumer.poll(timeout_ms=session_timeout * 1000, max_records=100, update_offsets=True)
 
         if not record:
             print("{} Seconds without new data!".format(session_timeout))
-            print("total points>>",total)
+            print("total points>>", total)
             if session_data:
-                print("Logging remaining data points into session ",session_number)
+                print("Logging remaining data points into session ", session_number)
                 dataset_name = "rov_prediction_{}".format(session_number)
-                log_session(dataset_name,session_data)
-                
+                log_session(dataset_name, session_data)
+
                 session_data = []
-                session_number+=1
-    
-        for k,v in record.items():
+                session_number += 1
+
+        for k, v in record.items():
             for row in v:
                 total += 1
                 current = row.value
-                current_ts = current['timestamp']
-                resid_window.append(current['residual'])
-                current = add_moving_averages(resid_window,current)
+                current_ts = current["timestamp"]
+                resid_window.append(current["residual"])
+                current = add_moving_averages(resid_window, current)
 
                 if not prev_ts:
                     prev_ts = current_ts
 
-                if abs(current_ts - prev_ts) > 5*60*1000:
-                    print("Logging session ",session_number)
+                if abs(current_ts - prev_ts) > 5 * 60 * 1000:
+                    print("Logging session ", session_number)
                     dataset_name = "rov_prediction_{}".format(session_number)
-                    log_session(dataset_name,session_data)                    
+                    log_session(dataset_name, session_data)
                     session_data = []
-                    session_number+=1
+                    session_number += 1
                 session_data.append(current)
                 prev_ts = current_ts
 
-def moving_average(slice_,wd):
+
+def moving_average(slice_, wd):
     if np.nan in slice_:
         return np.nan
     if len(slice_) != wd:
         return np.nan
-    return sum(slice_)/wd
+    return sum(slice_) / wd
 
-def add_moving_averages(resid_window,current):
+
+def add_moving_averages(resid_window, current):
 
     r_5 = list(resid_window)[-5:]
     r_10 = list(resid_window)[-10:]
     r_15 = list(resid_window)[-15:]
 
-    current['residual_m5'] = moving_average(r_5,5)
-    current['residual_m10'] = moving_average(r_10,10)
-    current['residual_m15'] = moving_average(r_15,15)
+    current["residual_m5"] = moving_average(r_5, 5)
+    current["residual_m10"] = moving_average(r_10, 10)
+    current["residual_m15"] = moving_average(r_15, 15)
 
     return current
 
 
-if (__name__=='__main__'):
+if __name__ == "__main__":
     main()
--- examples/rov_whylogs/telemetry_producer.py
+++ examples/rov_whylogs/telemetry_producer.py
@@ -1,53 +1,51 @@
 import json
 import os
+
 from kafkaConnector import create_producer
 
 producer = create_producer()
 
-foldername = 'rov_data'
+foldername = "rov_data"
 files = [str(i) for i in range(4)]
 
 sessions = []
-for ix,file in enumerate(files):
-    with open(os.path.join(foldername,'{}_injected.json'.format(file)),'r') as f:
+for ix, file in enumerate(files):
+    with open(os.path.join(foldername, "{}_injected.json".format(file)), "r") as f:
         raw_data = json.load(f)
-        sessions.append([{
-                        'mtarg1':str(x['mtarg1']),
-                        'mtarg2':str(x['mtarg2']),
-                        'mtarg3':str(x['mtarg3']),
-
-                        'roll':x['roll'],
-                        'pitch':x['pitch'],
-                        'yaw':x['yaw'],
-
-                        'LACCX':x['LACCX'],
-                        'LACCY':x['LACCY'],
-                        'LACCZ':x['LACCZ'],
-
-                        'GYROX':x['GYROX'],
-                        'GYROY':x['GYROY'],
-                        'GYROZ':x['GYROZ'],
-
-                        'SC1I':x['SC1I'],
-                        'SC2I':x['SC2I'],
-                        'SC3I':x['SC3I'],
-
-                        'BT1I':x['BT1I'],
-                        'BT2I':x['BT2I'],
-
-                        'vout':x['vout'],
-                        'iout':x['iout'],
-                        'cpuUsage':x['cpuUsage'],
-            
-
-                        'timestamp':x['timestamp']} for x in raw_data['0']['data']])
-
+        sessions.append(
+            [
+                {
+                    "mtarg1": str(x["mtarg1"]),
+                    "mtarg2": str(x["mtarg2"]),
+                    "mtarg3": str(x["mtarg3"]),
+                    "roll": x["roll"],
+                    "pitch": x["pitch"],
+                    "yaw": x["yaw"],
+                    "LACCX": x["LACCX"],
+                    "LACCY": x["LACCY"],
+                    "LACCZ": x["LACCZ"],
+                    "GYROX": x["GYROX"],
+                    "GYROY": x["GYROY"],
+                    "GYROZ": x["GYROZ"],
+                    "SC1I": x["SC1I"],
+                    "SC2I": x["SC2I"],
+                    "SC3I": x["SC3I"],
+                    "BT1I": x["BT1I"],
+                    "BT2I": x["BT2I"],
+                    "vout": x["vout"],
+                    "iout": x["iout"],
+                    "cpuUsage": x["cpuUsage"],
+                    "timestamp": x["timestamp"],
+                }
+                for x in raw_data["0"]["data"]
+            ]
+        )
 
 
 total = 0
 for session in sessions:
     for sample in session:
-        producer.send('telemetry-rov', sample)
+        producer.send("telemetry-rov", sample)
         producer.flush()
-        total+=1
-print("Total>>",total)
+        total += 1
+print("Total>>", total)
