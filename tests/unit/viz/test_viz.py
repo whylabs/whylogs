1702b7e3ca466378f0f213846084d4c07ed2f888
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2021-06-30 13:45:20 -0700

    add more unit tests.

--- tests/unit/viz/test_viz.py
+++ tests/unit/viz/test_viz.py
@@ -4,6 +4,9 @@ from uuid import uuid4
 from whylogs.core.datasetprofile import DatasetProfile
 from whylogs.viz import ProfileVisualizer
 
+import matplotlib.pyplot as _plt
+
+_plt.ioff()
 
 def test_viz():
     now = datetime.datetime.utcnow()
@@ -15,11 +18,18 @@ def test_viz():
         tags={"key": "value"},
         metadata={"key": "value"},
     )
+    x2 = DatasetProfile(
+        name="test",
+        session_id=uuid4().hex,
+        session_timestamp=now+datetime.timedelta(days=1),
+        tags={"key": "value"},
+        metadata={"key": "value"},
+    )
     x1.track("col1", "value")
     viz = ProfileVisualizer()
     viz.available_plots()
 
-    viz.set_profiles([x1])
+    viz.set_profiles([x1,x2])
 
 
 def test_viz_distribution(profile_lending_club):
@@ -27,6 +37,7 @@ def test_viz_distribution(profile_lending_club):
     viz = ProfileVisualizer()
     viz.set_profiles([profile_lending_club])
     viz.plot_distribution("loan_amnt")
+    
 
 
 def test_viz_datatype(profile_lending_club):
@@ -35,7 +46,7 @@ def test_viz_datatype(profile_lending_club):
     viz.set_profiles([profile_lending_club])
 
     viz.plot_data_types("emp_length")
-
+    
 
 def test_viz_uniqueness(profile_lending_club):
 
@@ -43,7 +54,30 @@ def test_viz_uniqueness(profile_lending_club):
     viz.set_profiles([profile_lending_club])
 
     viz.plot_uniqueness("max_bal_bc")
+    
 
+def test_viz_string_length(profile_lending_club):
+
+    viz = ProfileVisualizer()
+    viz.set_profiles([profile_lending_club])
+
+    viz.plot_string_length("url")
+
+
+def test_viz_token_length(profile_lending_club):
+
+    viz = ProfileVisualizer()
+    viz.set_profiles([profile_lending_club])
+
+    viz.plot_token_length("url",character_list="htp")
+
+def test_viz_char_pos(profile_lending_club):
+
+    viz = ProfileVisualizer()
+    viz.set_profiles([profile_lending_club])
+
+    viz.plot_char_pos("url",character_list="htp")
+    
 
 def test_viz_missing_values(profile_lending_club):
 
@@ -51,3 +85,4 @@ def test_viz_missing_values(profile_lending_club):
     viz.set_profiles([profile_lending_club])
 
     viz.plot_missing_values("hardship_payoff_balance_amount")
+    
