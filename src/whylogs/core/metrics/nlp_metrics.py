697ea928f360fd207d1046897355fbf48adc793b
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2021-06-29 19:51:10 -0700

    format fixes
    fix imports
    
    add missing type
    
    removed unused variable
    
    format fix

--- src/whylogs/core/metrics/nlp_metrics.py
+++ src/whylogs/core/metrics/nlp_metrics.py
@@ -1,11 +1,9 @@
-import math
-from typing import List
+from typing import List, Union
 
 import jiwer
 
-from whylogs.proto import RegressionMetricsMessage
-
-SUPPORTED_TYPES = "regression"
+from whylogs.core.statistics import NumberTracker
+from whylogs.proto import NLPMetricsMessage
 
 
 class NLPMetrics:
@@ -13,9 +11,9 @@ class NLPMetrics:
         self.prediction_field = prediction_field
         self.target_field = target_field
 
-    def update(self, predictions: Union[List[str], str], targets: Union[List[str]], transform=None)->None:
+    def update(self, predictions: Union[List[str], str], targets: Union[List[str]], transform=None) -> None:
         """
-        Function adds predictions and targets computation of regression metrics.
+        Function adds predictions and targets computation of nlp metrics.
 
         Args:
             predictions (Union[str,List[str]]):
@@ -23,15 +21,15 @@ class NLPMetrics:
 
         """
         if transform:
-            mes= jiwer.compute_measures(truth=targets, hypothesis=predictions, truth_transform=transform, hypothesis_transform=transform)
+            mes = jiwer.compute_measures(truth=targets, hypothesis=predictions, truth_transform=transform, hypothesis_transform=transform)
         else:
-            mes= jiwer.compute_measures(truth=targets, hypothesis=predictions)
+            mes = jiwer.compute_measures(truth=targets, hypothesis=predictions)
 
         self.mer.update(mes["mer"])
         self.wer.update(mes["wer"])
         self.wil.update(mes["wil"])
 
-    def merge(self, other : 'NLPMetrics')->'NLPMetrics':
+    def merge(self, other: "NLPMetrics") -> "NLPMetrics":
         """
         Merge two seperate nlp metrics
 
@@ -43,12 +41,12 @@ class NLPMetrics:
         if other is None:
             return self
 
-        return new_reg
-
-        merged_nlp_metrics= NLPMetrics()
+        merged_nlp_metrics = NLPMetrics()
         merged_nlp_metrics.mer.merge(other.mer)
         merged_nlp_metrics.wer.merge(other.wer)
         merged_nlp_metrics.wil.merge(other.wil)
+
+        return merged_nlp_metrics
 
     def to_protobuf(
         self,
