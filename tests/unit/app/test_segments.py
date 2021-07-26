18945882b4914e45bb5a4c96163f6f91cbce63c7
Andy Dang🤖 <andy@whylabs.ai>
2021-07-26 13:34:16 -0700

    format-fix: linting

--- tests/unit/app/test_segments.py
+++ tests/unit/app/test_segments.py
@@ -3,10 +3,10 @@ import json
 import os
 import shutil
 
+import pandas as pd
 import pytest
 from freezegun import freeze_time
 from pandas import util
-import pandas as pd
 
 from whylogs.app.config import SessionConfig, WriterConfig
 from whylogs.app.session import session_from_config
@@ -22,12 +22,12 @@ def test_segments(df_lending_club, tmpdir):
     session_config = SessionConfig("project", "pipeline", writers=[writer_config])
     with session_from_config(session_config) as session:
         with session.logger(
-                "test",
-                segments=[
-                    [{"key": "home_ownership", "value": "RENT"}],
-                    [{"key": "home_ownership", "value": "MORTGAGE"}],
-                ],
-                cache_size=1,
+            "test",
+            segments=[
+                [{"key": "home_ownership", "value": "RENT"}],
+                [{"key": "home_ownership", "value": "MORTGAGE"}],
+            ],
+            cache_size=1,
         ) as logger:
             logger.log_dataframe(df_lending_club)
             profile = logger.profile
@@ -36,10 +36,8 @@ def test_segments(df_lending_club, tmpdir):
 
     assert profile is None
     assert len(profiles) == 2
-    assert profiles[list(profiles.keys())[0]].tags["segment"] == json.dumps(
-        [{"key": "home_ownership", "value": "RENT"}])
-    assert profiles[list(profiles.keys())[1]].tags["segment"] == json.dumps(
-        [{"key": "home_ownership", "value": "MORTGAGE"}])
+    assert profiles[list(profiles.keys())[0]].tags["segment"] == json.dumps([{"key": "home_ownership", "value": "RENT"}])
+    assert profiles[list(profiles.keys())[1]].tags["segment"] == json.dumps([{"key": "home_ownership", "value": "MORTGAGE"}])
     check_segment = profiles[list(profiles.keys())[1]]
     assert mortage_segment == check_segment
 
@@ -95,11 +93,11 @@ def test_segments_with_rotation(df_lending_club, tmpdir):
     with freeze_time("2012-01-14 03:21:34", tz_offset=-4) as frozen_time:
         session = session_from_config(session_config)
         with session.logger(
-                "test",
-                with_rotation_time="s",
-                segments=["home_ownership"],
-                profile_full_dataset=True,
-                cache_size=1,
+            "test",
+            with_rotation_time="s",
+            segments=["home_ownership"],
+            profile_full_dataset=True,
+            cache_size=1,
         ) as logger:
             logger.log_dataframe(df_lending_club)
             frozen_time.tick(delta=datetime.timedelta(seconds=1))
@@ -128,8 +126,7 @@ def test_one_segment(tmpdir, image_files):
 
     session = session_from_config(session_config)
 
-    df = pd.DataFrame(data={'x': [1], 'y': [4],
-                            'z': [0.1]})
+    df = pd.DataFrame(data={"x": [1], "y": [4], "z": [0.1]})
     with session.logger("segment_test", segments=["x", "y"]) as logger:
         logger.log_segments(df)
         assert len(logger.segmented_profiles) == 1
@@ -146,8 +143,7 @@ def test_log_multiple_segments(tmpdir, image_files):
 
     session = session_from_config(session_config)
 
-    df = pd.DataFrame(data={'x': [1, 2, 3, 1, 2, 3, 1, 2, 3], 'y': [4, 5, 6, 5, 6, 4, 6, 4, 5],
-                            'z': [0.1, 0.2, 0.3, 0.1, 0.2, 0.3, 0.1, 0.2, 0.3]})
+    df = pd.DataFrame(data={"x": [1, 2, 3, 1, 2, 3, 1, 2, 3], "y": [4, 5, 6, 5, 6, 4, 6, 4, 5], "z": [0.1, 0.2, 0.3, 0.1, 0.2, 0.3, 0.1, 0.2, 0.3]})
     with session.logger("image_test", segments=["x", "y"]) as logger:
         logger.log_segments(df)
         assert len(logger.segmented_profiles) == 9
