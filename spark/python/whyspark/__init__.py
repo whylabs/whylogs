a076cc19f95207e1a97ead749fb01a8d3eb9bdfa
Bernease Herman <bernease@gmail.com>
2021-07-30 13:19:45 -0700

    PySpark autosegmentation and tests (#36)
    * [WIP] Adding dependencies to the python folder
    
    This is a POC to get unit testing enabled in a sane way for the python
    code that we have. Running `make install test` should get the sample
    test running. After, we can make sure to run that command during the
    build as well.
    
    * Fixed typo for withRegressionModel method (#35)
    * Add PySpark autosegmentation and tests
    
    Co-authored-by: Anthony Naddeo <anthony@whylabs.ai>
    Co-authored-by: Leandro G. Almeida <leandro.g.almeida@gmail.com>

--- spark/python/whyspark/__init__.py
+++ spark/python/whyspark/__init__.py
@@ -1,6 +1,8 @@
 from .udt import WhyProfileSession, new_profiling_session
+from .preprocessing import estimate_segments
 
 __ALL__ = [
     WhyProfileSession,
     new_profiling_session,
+    estimate_segments
 ]
