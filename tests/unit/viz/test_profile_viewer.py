e5723bbc69ffe3327a2c0922fb53649f58fdbf53
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2021-08-22 15:09:08 -0700

    fix style

--- tests/unit/viz/test_profile_viewer.py
+++ tests/unit/viz/test_profile_viewer.py
@@ -1,5 +1,6 @@
-import os
 import datetime
+import os
+
 import numpy as np
 
 from whylogs import get_or_create_session
@@ -7,14 +8,13 @@ from whylogs.viz import profile_viewer
 
 
 def test_profile_viewer(tmpdir):
-    _session=None
     session = get_or_create_session()
 
-    with session.logger("mytestytest",dataset_timestamp=datetime.datetime(2021, 6, 2)) as logger:
+    with session.logger("mytestytest", dataset_timestamp=datetime.datetime(2021, 6, 2)) as logger:
         for _ in range(5):
-            logger.log({"uniform_integers": np.random.randint(0,50)})
+            logger.log({"uniform_integers": np.random.randint(0, 50)})
             logger.log({"nulls": None})
 
-        profile=logger.profile
-    profile_viewer(profiles=[profile],output_path=tmpdir+"my_test.html")
-    assert os.path.exists(tmpdir+"my_test.html")
+        profile = logger.profile
+    profile_viewer(profiles=[profile], output_path=tmpdir + "my_test.html")
+    assert os.path.exists(tmpdir + "my_test.html")
