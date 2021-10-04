0bf090825dd514857d856cf08c7ddb63ff153525
Jamie Broomall <88007022+jamie256@users.noreply.github.com>
2021-10-04 16:40:12 -0700

    Add warning on larger dataframe logging, add perf test and fixture for conditional tests w/ --load (#326)
    * Add test fixture for conditionally running test if --load_test is passed, and add a test to profile log_dataframe
    
    * Add warning message if logging a dataframe larger than 200k elements

--- tests/component/conftest.py
+++ tests/component/conftest.py
@@ -3,14 +3,30 @@ import sys
 
 import pytest
 
-import whylogs
-
 _MY_DIR = os.path.realpath(os.path.dirname(__file__))
 # Allow import of the test utilities packages
 sys.path.insert(0, os.path.join(_MY_DIR, os.pardir, "helpers"))
 # Test the parent package
 sys.path.insert(0, os.path.join(_MY_DIR, os.pardir))
 # Verify whylogs is importable
+
+
+def pytest_addoption(parser):
+    parser.addoption("--load", action="store_true", default=False, help="run load tests")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "load: mark test as load to skip running with unit tests")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--load"):
+        # --integ specified on command line: do not skip integ tests
+        return
+    skip_load_test = pytest.mark.skip(reason="need --load option to run")
+    for item in items:
+        if "load" in item.keywords:
+            item.add_marker(skip_load_test)
 
 
 @pytest.fixture(scope="session")
