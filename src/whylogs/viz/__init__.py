b3a3dac9f63b050ff3213f96e77a4dc36b141ae3
ssxcho <xchton@gmail.com>
2022-01-20 18:42:56 +0400

    change vizualisation stucture, add download and view feature double histogram

--- src/whylogs/viz/__init__.py
+++ src/whylogs/viz/__init__.py
@@ -1,5 +1,6 @@
 from .browser_viz import profile_viewer
-from .jupyter_notebook_viz import display_profile
+from .jupyter_notebook_viz import DisplayProfile
+
 from .visualizer import BaseProfileVisualizer, ProfileVisualizer
 
-__ALL__ = [ProfileVisualizer, BaseProfileVisualizer, profile_viewer, display_profile]
+__ALL__ = [ProfileVisualizer, BaseProfileVisualizer, DisplayProfile, profile_viewer]
--- src/whylogs/viz/jupyter_notebook_viz.py
+++ src/whylogs/viz/jupyter_notebook_viz.py
@@ -2,10 +2,12 @@ import logging
 import os
 import tempfile
 import webbrowser
+import sys
+import json
 from typing import List
 import html
-import IPython
 
+from IPython.core.display import display, HTML
 from whylogs.core import DatasetProfile
 from whylogs.util.protobuf import message_to_json
 
@@ -15,49 +17,129 @@ _MY_DIR = os.path.realpath(os.path.dirname(__file__))
 logger = logging.getLogger(__name__)
 
 
-def display_profile(profiles: List[DatasetProfile] = None, reference_profiles: List[DatasetProfile] = None) -> str:
-    """
-    display profile in jupyter notebook
-    """
-    try:
-        from pybars import Compiler
-    except ImportError as e:
-        Compiler = None
-        logger.debug(str(e))
-        logger.debug(
-            "Unable to load pybars; install pybars3 to load profile from directly from the current session ")
+class DisplayProfile:
 
-    # create json output from profiles
-    if profiles:
-        if len(profiles) > 1:
+    def __init__(self, profiles: List[DatasetProfile] = None, reference_profiles: List[DatasetProfile] = None):
+
+        self.profiles = profiles
+        self.reference_profiles = reference_profiles
+
+    def __display_html(self, output_index, width_height):
+        # convert html to iframe and return it wrapped in Ipython...HTML()
+        iframe = f'''<iframe srcdoc="{html.escape(output_index)}" {width_height} frameBorder=0></iframe>'''
+        return HTML(iframe)
+
+    def __iframe_output_sizing(self, html):
+        # add all required heights and widths for individual HTMLs to be displayed in notebook
+        sizes = {'index-hbs-cdn-all-in-for-jupyter-notebook.html': 'width=960px height=1000px',
+                 'index-hbs-cdn-all-in-jupyter-distribution-chart.html': 'width=960px height=277px'
+                 }
+        return str(sizes.get(html))
+
+    def summary(self) -> str:
+        """
+        create profile html
+        """
+        try:
+            from pybars import Compiler
+        except ImportError as e:
+            Compiler = None
+            logger.debug(str(e))
+            logger.debug(
+                "Unable to load pybars; install pybars3 to load profile from directly from the current session ")
+        # create json output from profiles
+        if self.profiles:
+            if len(self.profiles) > 1:
+                logger.warning(
+                    "More than one profile not implemented yet, default to first profile in the list ")
+            profile_jsons = [message_to_json(each_prof.to_summary())
+                             for each_prof in self.profiles]
+
+            if self.reference_profiles:
+                reference_profile_jsons = [message_to_json(each_prof.to_summary())
+                                           for each_prof in self.reference_profiles]
+
+        else:
             logger.warning(
-                "More than one profile not implemented yet, default to first profile in the list ")
-        profile_jsons = [message_to_json(each_prof.to_summary()) for each_prof in profiles]
-        if reference_profiles:
-            reference_profile_jsons = [message_to_json(each_prof.to_summary())
-                                       for each_prof in reference_profiles]
+                "Got no profile data, make sure you pass data correctly ")
+            return None
+        index_path = os.path.abspath(os.path.join(
+            _MY_DIR, os.pardir, "viewer", "index-hbs-cdn-all-in-for-jupyter-notebook.html"))
 
-    else:
-        logger.warning(
-            "Got no profile data, make sure you pass data correctly ")
-        return None
+        html_file = self.__iframe_output_sizing("index-hbs-cdn-all-in-for-jupyter-notebook.html")
+        with open(index_path, "r") as file_with_template:
+            source = file_with_template.read()
+        # compile templated files
+        compiler = Compiler()
+        template = compiler.compile(source)
+        # replace handlebars for json profiles
+        if self.reference_profiles:
+            output_index = template(
+                {"profile_from_whylogs": profile_jsons[0], "reference_profile": reference_profile_jsons[0]})
+        else:
+            output_index = template(
+                {"profile_from_whylogs": profile_jsons[0]})
+        return self.__display_html(output_index, html_file)
 
-    index_path = os.path.abspath(os.path.join(
-        _MY_DIR, os.pardir, "viewer", "index-hbs-cdn-all-in-for-jupyter-notebook.html"))
+    def download(self, html, path=None):
+        # code to write html arg to file and generate name using TimeStamp
+        if path:
+            output_path = os.path.abspath(os.path.expanduser(path))
+        else:
+            output_path = 'html_reports/'
 
-    with open(index_path, "r") as file_with_template:
-        source = file_with_template.read()
+        name_of_file = "test"
 
-    # compile templated files
-    compiler = Compiler()
-    template = compiler.compile(source)
-    # replace handlebars for json profiles
-    if reference_profiles:
-        output_index = template(
-            {"profile_from_whylogs": profile_jsons[0], "reference_profile": reference_profile_jsons[0]})
-    else:
-        output_index = template(
-            {"profile_from_whylogs": profile_jsons[0]})
+        completeName = os.path.join(output_path, name_of_file+".html")
 
-    iframe = f'''<iframe srcdoc="{html.escape(output_index)}" width=960px height=1000px frameBorder=0></iframe>'''
-    return IPython.display.HTML(iframe)
+        file1 = open(completeName, "w")
+
+        file1.write(html.data)
+
+        file1.close()
+
+    def feature(self, name):
+        print(self.profiles[0].dataset_timestamp)
+        """
+        create profile html
+        """
+        try:
+            from pybars import Compiler
+        except ImportError as e:
+            Compiler = None
+            logger.debug(str(e))
+            logger.debug(
+                "Unable to load pybars; install pybars3 to load profile from directly from the current session ")
+        # create json output from profiles
+        if self.profiles:
+            if len(self.profiles) > 1:
+                logger.warning(
+                    "More than one profile not implemented yet, default to first profile in the list ")
+            profile_jsons = [message_to_json(each_prof.to_summary())
+                             for each_prof in self.profiles]
+
+            if self.reference_profiles:
+                reference_profile_jsons = [message_to_json(each_prof.to_summary())
+                                           for each_prof in self.reference_profiles]
+
+        else:
+            logger.warning(
+                "Got no profile data, make sure you pass data correctly ")
+            return None
+        index_path = os.path.abspath(os.path.join(
+            _MY_DIR, os.pardir, "viewer", "index-hbs-cdn-all-in-jupyter-distribution-chart.html"))
+
+        html_file = self.__iframe_output_sizing(
+            "index-hbs-cdn-all-in-jupyter-distribution-chart.html")
+        with open(index_path, "r") as file_with_template:
+            source = file_with_template.read()
+            # compile templated files
+        compiler = Compiler()
+        template = compiler.compile(source)
+        # replace handlebars for json profiles
+        if self.reference_profiles:
+            profile_feature = json.loads(profile_jsons[0])
+            reference_profile_feature = json.loads(reference_profile_jsons[0])
+            output_index = template(
+                {"profile_from_whylogs": json.dumps(profile_feature.get('columns').get(name)), "reference_profile": json.dumps(reference_profile_feature.get('columns').get(name))})
+        return self.__display_html(output_index, html_file)
