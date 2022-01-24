69fb0ecebd3f55bd8794aa4fbfb86335b230b2b3
Richard Rogers <rrogers@opendius.net>
2022-01-24 02:53:32 -0800

    add test program

---
+++ tests/smoketest.py
@@ -0,0 +1,13 @@
+
+"""
+This is here to verify that the produced wheel includes
+all the necessary dependencies. This is excersized by
+the CI workflow and does not use pytest because it is
+intended to test the wheel in a production environment,
+not a development environment.
+"""
+
+from whylogs.core import ColumnProfile
+from whylogs.core.statistics.hllsketch import HllSketch
+from whylogs.proto import ColumnMessage, ColumnSummary, InferredType
+from whylogs.util.protobuf import message_to_dict
