2000e8476eaa7af54837094ae1d12846782bd7fc
Richard Rogers <rrogers@opendius.net>
2022-01-25 11:34:18 -0800

    better test code

--- tests/smoketest.py
+++ tests/smoketest.py
@@ -1,12 +1,26 @@
 """
 This is here to verify that the produced wheel includes
-all the necessary dependencies. This is excersized by
+all the necessary dependencies. This is exercised by
 the CI workflow and does not use pytest because it is
 intended to test the wheel in a production environment,
 not a development environment.
 """
 
-from whylogs.core import ColumnProfile
-from whylogs.core.statistics.hllsketch import HllSketch
-from whylogs.proto import ColumnMessage, ColumnSummary, InferredType
-from whylogs.util.protobuf import message_to_dict
+import os
+
+import pandas as pd
+
+import whylogs
+from whylogs import get_or_create_session
+from whylogs.app import Session
+from whylogs.app.writers import WhyLabsWriter
+
+writer = WhyLabsWriter()
+session = Session(writers=[writer])
+
+whylogs_session = get_or_create_session()
+
+print(session)
+print(whylogs_session)
+print("Successfully created whylogs session with version:")
+print(whylogs.__version__)
