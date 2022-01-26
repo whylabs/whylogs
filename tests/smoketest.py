1df51e97a78f880a79dd59bfeca450baa9efa9a5
richard-rogers <93153899+richard-rogers@users.noreply.github.com>
2022-01-26 13:46:22 -0800

    Integrate wheel smoke test into standard CI workflow. (#8)
    * Integrate wheel smoke test into standard CI workflow.
    
    Co-authored-by: Richard Rogers <rrogers@opendius.net>

---
+++ tests/smoketest.py
@@ -0,0 +1,26 @@
+from whylogs_v1 import __version__, package_version
+
+"""
+This is here to verify that the produced wheel includes
+all the necessary dependencies. This is excersized by
+the CI workflow and does not use pytest because it is
+intended to test the wheel in a production environment,
+not a development environment.
+"""
+
+current_version = "1.0.0"
+
+
+def test_package_version() -> None:
+    """Test package version calculation."""
+    assert package_version() == current_version
+
+
+def test_package_version_not_found() -> None:
+    """Test package version calculation when package is not installed."""
+    assert package_version(package="incorrect") == "incorrect is not installed."
+
+
+def test_version() -> None:
+    """Test package version number."""
+    assert __version__ == current_version
