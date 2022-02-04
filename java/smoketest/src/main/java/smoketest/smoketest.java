e41f4cfeff884915b93ecbae5ac89f0eaa1ac770
Richard Rogers <rrogers@opendius.net>
2022-02-04 05:13:47 -0800

    Add Java package smoketest

---
+++ java/smoketest/src/main/java/smoketest/smoketest.java
@@ -0,0 +1,8 @@
+import com.whylogs.core;
+import java.time.Instant;
+
+public class SmokeTest {
+    public static void main(String[] args) {
+        val dsp = new DatasetProfile("test", Instant.now());
+    }
+}
