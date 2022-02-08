a3b879bc843f29d29e199a9c5dcdac7a44ff8215
Jirayr-Solvee <jirayr.melikyan@yahoo.com>
2022-02-09 00:09:46 +0400

    add test for viewer

--- tests/unit/viz/test_profile_viewer.py
+++ tests/unit/viz/test_profile_viewer.py
@@ -8,7 +8,7 @@ from whylogs.app.session import session_from_config
 from whylogs.viz import profile_viewer
 
 
-def test_profile_viewer(tmpdir, local_config_path):
+def test_profile_viewer_without_reference(tmpdir, local_config_path):
 
     config = load_config(local_config_path)
     session = session_from_config(config)
@@ -22,3 +22,19 @@ def test_profile_viewer(tmpdir, local_config_path):
     result = profile_viewer(profiles=[profile], output_path=tmpdir + "my_test.html")
     assert os.path.exists(tmpdir + "my_test.html")
     assert result == tmpdir + "my_test.html"
+
+
+def test_profile_viewer_with_reference(tmpdir, local_config_path):
+
+    config = load_config(local_config_path)
+    session = session_from_config(config)
+
+    with session.logger("mytestytest", dataset_timestamp=datetime.datetime(2021, 6, 2)) as logger:
+        for _ in range(5):
+            logger.log({"uniform_integers": np.random.randint(0, 50)})
+            logger.log({"nulls": None})
+
+        profile = logger.profile
+    result = profile_viewer(profiles=[profile], reference_profiles=[profile], output_path=tmpdir + "my_test_ref.html")
+    assert os.path.exists(tmpdir + "my_test_ref.html")
+    assert result == tmpdir + "my_test_ref.html"
