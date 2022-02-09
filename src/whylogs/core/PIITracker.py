7cd86abab79f3a3509aa6157109938701dacbc20
MilenaTrajanoska <trajanoska28@gmail.com>
2022-02-09 15:42:39 +0100

    Move PIITracker to separate file

---
+++ src/whylogs/core/PIITracker.py
@@ -0,0 +1,42 @@
+from whylogs.core.statistics.constraints import (
+    ValueConstraints,
+    containsEmailConstraint,
+    containsCreditCardConstraint,
+    containsSSNConstraint,
+)
+
+
+class PIITracker:
+    def __init__(self, column_name, reporting_threshold=1):
+        self.column_name = column_name
+        self.reporting_threshold = reporting_threshold
+        self.pii_constraints = ValueConstraints([
+                containsEmailConstraint(
+                    name=f"The column {self.column_name} contains some values identified as e-mail addresses"
+                ),
+                containsCreditCardConstraint(
+                    name=f"The column {self.column_name} contains some values identified as credit card numbers"
+                ),
+                containsSSNConstraint(
+                    name=f"The column {self.column_name} contains some values identified as social security numbers"
+                ),
+            ])
+        self.tracking = True
+
+    def update(self, value):
+        if self.tracking:
+            self.pii_constraints.update(value)
+            for constraint in self.pii_constraints.raw_value_constraints:
+                if (constraint.total - constraint.failures) > self.reporting_threshold:
+                    self.tracking = False
+
+    def give_pii_insights(self):
+        pii_report = []
+        for constraint in self.pii_constraints.raw_value_constraints:
+            if (constraint.total - constraint.failures) > self.reporting_threshold:
+                pii_report.append(constraint.name)
+
+        if len(pii_report) > 0:
+            return pii_report
+        else:
+            return f"There was no information identified as PII in the column {self.column_name}"
