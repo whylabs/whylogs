Fix Chi-Squared statistic calculation

src/whylogs/core/summaryconverters.py
@@ -315,13 +315,14 @@ def compute_chi_squared_test_p_value(target_distribution: ReferenceDistributionD
 
     chi_sq = 0
     for item in target_freq_items.items:
-        i_frequency = item.estimate / target_total_count
-        ref_frequency = ref_dist_items[item.json_value] / ref_total_count if item.json_value in ref_dist_items.keys() else 0
+        target_frequency = item.estimate
+        ref_frequency = ref_dist_items[item.json_value]
         if ref_frequency == 0:
             chi_sq = np.inf
         else:
-            chi_sq += (i_frequency - ref_frequency) ** 2 / ref_frequency
+            chi_sq += (target_frequency - ref_frequency) ** 2 / ref_frequency
 
     degrees_of_freedom = target_unique_count - 1
+    degrees_of_freedom = degrees_of_freedom if degrees_of_freedom > 0 else 1
     p_value = stats.chi2.sf(chi_sq, degrees_of_freedom)
     return type("Object", (), {"chi_squared_test": p_value})