3e68e68b2b6b050e7dfc0d21f505333f3049ccef
Richard Rogers <rrogers@opendius.net>
2022-02-11 16:01:48 -0800

    Exercise protobuf

--- java/smoketest/src/main/java/smoketest/SmokeTest.java
+++ java/smoketest/src/main/java/smoketest/SmokeTest.java
@@ -1,8 +1,11 @@
 import com.whylogs.core.DatasetProfile;
+import com.whylogs.core.statistics.NumberTracker;
 import java.time.Instant;
 
 public class SmokeTest {
     public static void main(String[] args) {
        DatasetProfile dsp = new DatasetProfile("test", Instant.now());
+       NumberTracker tracker = new NumberTracker();
+       val protobuf = tracker.toProtobuf();
     }
 }
