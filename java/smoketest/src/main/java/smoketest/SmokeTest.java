e6c45485080dbbacedf6524a3b1861c13049f455
Richard Rogers <rrogers@opendius.net>
2022-02-11 16:07:35 -0800

    import message type

--- java/smoketest/src/main/java/smoketest/SmokeTest.java
+++ java/smoketest/src/main/java/smoketest/SmokeTest.java
@@ -1,11 +1,12 @@
 import com.whylogs.core.DatasetProfile;
 import com.whylogs.core.statistics.NumberTracker;
+import com.whylogs.core.message.NumbersMessage;
 import java.time.Instant;
 
 public class SmokeTest {
     public static void main(String[] args) {
        DatasetProfile dsp = new DatasetProfile("test", Instant.now());
        NumberTracker tracker = new NumberTracker();
-       val protobuf = tracker.toProtobuf();
+       NumbersMessage.Builder protobuf = tracker.toProtobuf();
     }
 }
