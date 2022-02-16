611255ee330f159abd9fde329ab5da7e8420332c
Jirayr-Solvee <jirayr.melikyan@yahoo.com>
2022-02-16 21:14:19 +0400

    add sum, change colors

--- src/whylogs/viz/jupyter_notebook_viz.py
+++ src/whylogs/viz/jupyter_notebook_viz.py
@@ -1,19 +1,20 @@
+import html
+import json
 import logging
 import os
-import sys
-import json
-import html
 from typing import List
-from IPython.core.display import display, HTML
+
+from IPython.core.display import HTML
 
 from whylogs.core import DatasetProfile
 from whylogs.util.protobuf import message_to_json
 
 from .utils.profile_viz_calculations import (
     add_drift_val_to_ref_profile_json,
-    calculate_variance,
     calculate_coefficient_of_variation,
-    calculate_quantile_statistics_for_single_feature
+    calculate_quantile_statistics_for_single_feature,
+    calculate_sum,
+    calculate_variance,
 )
 
 _MY_DIR = os.path.realpath(os.path.dirname(__file__))
@@ -22,15 +23,15 @@ logger = logging.getLogger(__name__)
 
 
 class NotebookProfileViewer:
-    SUMMARY_REPORT_TEMPLATE_NAME = 'index-hbs-cdn-all-in-for-jupyter-notebook.html'
-    DOUBLE_HISTOGRAM_TEMPLATE_NAME = 'index-hbs-cdn-all-in-jupyter-distribution-chart.html'
-    FEATURE_STATISTICS_TEMPLATE_NAME = 'index-hbs-cdn-all-in-jupyter-feature-summary-statistics.html'
-    CONSTRAINTS_REPORT_TEMPLATE_NAME = 'index-hbs-cdn-all-in-jupyter-constraints-report.html'
+    SUMMARY_REPORT_TEMPLATE_NAME = "index-hbs-cdn-all-in-for-jupyter-notebook.html"
+    DOUBLE_HISTOGRAM_TEMPLATE_NAME = "index-hbs-cdn-all-in-jupyter-distribution-chart.html"
+    FEATURE_STATISTICS_TEMPLATE_NAME = "index-hbs-cdn-all-in-jupyter-feature-summary-statistics.html"
+    CONSTRAINTS_REPORT_TEMPLATE_NAME = "index-hbs-cdn-all-in-jupyter-constraints-report.html"
     PAGE_SIZES = {
-        SUMMARY_REPORT_TEMPLATE_NAME: '1000px',
-        DOUBLE_HISTOGRAM_TEMPLATE_NAME: '277px',
-        FEATURE_STATISTICS_TEMPLATE_NAME: '650px',
-        CONSTRAINTS_REPORT_TEMPLATE_NAME: '750PX'
+        SUMMARY_REPORT_TEMPLATE_NAME: "1000px",
+        DOUBLE_HISTOGRAM_TEMPLATE_NAME: "277px",
+        FEATURE_STATISTICS_TEMPLATE_NAME: "650px",
+        CONSTRAINTS_REPORT_TEMPLATE_NAME: "750PX",
     }
 
     def __init__(self, target_profiles: List[DatasetProfile] = None, reference_profiles: List[DatasetProfile] = None):
@@ -39,27 +40,16 @@ class NotebookProfileViewer:
         # create json output from profiles
         if self.target_profiles:
             if len(self.target_profiles) > 1:
-                logger.warning(
-                    "More than one profile not implemented yet, default to first profile in the list "
-                )
-            self.target_profile_jsons = [
-                message_to_json(each_prof.to_summary()) for each_prof in self.target_profiles
-            ]
+                logger.warning("More than one profile not implemented yet, default to first profile in the list ")
+            self.target_profile_jsons = [message_to_json(each_prof.to_summary()) for each_prof in self.target_profiles]
             if self.reference_profiles:
-                self.reference_profile_jsons = [
-                    message_to_json(each_prof.to_summary()) for each_prof in self.reference_profiles
-                ]
+                self.reference_profile_jsons = [message_to_json(each_prof.to_summary()) for each_prof in self.reference_profiles]
         else:
-            logger.warning(
-                "Got no profile data, make sure you pass data correctly ")
+            logger.warning("Got no profile data, make sure you pass data correctly ")
             return None
 
     def __get_template_path(self, html_file_name):
-        template_path = os.path.abspath(
-            os.path.join(
-                _MY_DIR, os.pardir, "viewer", html_file_name
-            )
-        )
+        template_path = os.path.abspath(os.path.join(_MY_DIR, os.pardir, "viewer", html_file_name))
         return template_path
 
     def __get_compiled_template(self, template_name):
@@ -69,9 +59,7 @@ class NotebookProfileViewer:
         except ImportError as e:
             Compiler = None
             logger.debug(str(e))
-            logger.debug(
-                "Unable to load pybars; install pybars3 to load profile from directly from the current session "
-            )
+            logger.debug("Unable to load pybars; install pybars3 to load profile from directly from the current session ")
         with open(template_path, "r") as file_with_template:
             source = file_with_template.read()
         # compile templated files
@@ -82,96 +70,64 @@ class NotebookProfileViewer:
     def __pull_feature_data(self, profile, profile_jsons, feature_name):
         profile_features = json.loads(profile_jsons[0])
         feature_data = {}
-        feature_data['properties'] = profile_features.get('properties')
-        feature_data[feature_name] = profile_features.get('columns').get(feature_name)
-        feature_data[feature_name]['variance'] = calculate_variance(
-            profile_features,
-            feature_name
-        )
-        feature_data[feature_name]['coefficient_of_variation'] = calculate_coefficient_of_variation(
-            profile_features,
-            feature_name
-        )
-        feature_data[feature_name]['quantile_statistics'] = calculate_quantile_statistics_for_single_feature(
-            profile,
-            profile_features,
-            feature_name
-        )
+        feature_data["properties"] = profile_features.get("properties")
+        feature_data[feature_name] = profile_features.get("columns").get(feature_name)
+        feature_data[feature_name]["sum"] = calculate_sum(profile_features, feature_name)
+        feature_data[feature_name]["variance"] = calculate_variance(profile_features, feature_name)
+        feature_data[feature_name]["coefficient_of_variation"] = calculate_coefficient_of_variation(profile_features, feature_name)
+        feature_data[feature_name]["quantile_statistics"] = calculate_quantile_statistics_for_single_feature(profile, profile_features, feature_name)
         return feature_data
 
     def __display_rendered_template(self, template, template_name, height):
         if not height:
             height = self.PAGE_SIZES[template_name]
         # convert html to iframe and return it wrapped in Ipython...HTML()
-        iframe = f'''<div></div><iframe srcdoc="{html.escape(template)}" width=100% height={height} frameBorder=0></iframe>'''
+        iframe = f"""<div></div><iframe srcdoc="{html.escape(template)}" width=100% height={height} frameBorder=0></iframe>"""
         return HTML(iframe)
 
     def summary_drift_report(self, preferred_cell_height=None):
-        reference_profile = add_drift_val_to_ref_profile_json(
-            self.target_profiles[0],
-            self.reference_profiles[0],
-            json.loads(self.target_profile_jsons[0])
-        )
+        reference_profile = add_drift_val_to_ref_profile_json(self.target_profiles[0], self.reference_profiles[0], json.loads(self.target_profile_jsons[0]))
         template = self.__get_compiled_template(self.SUMMARY_REPORT_TEMPLATE_NAME)
         profiles_summary = {"profile_from_whylogs": self.target_profile_jsons[0]}
         if self.reference_profiles:
             profiles_summary["reference_profile_from_whylogs"] = json.dumps(reference_profile)
-        return self.__display_rendered_template(
-            template(profiles_summary),
-            self.SUMMARY_REPORT_TEMPLATE_NAME,
-            preferred_cell_height
-        )
+        return self.__display_rendered_template(template(profiles_summary), self.SUMMARY_REPORT_TEMPLATE_NAME, preferred_cell_height)
 
     def double_histogram(self, feature_names, preferred_cell_height=None):
         if type(feature_names) is not list:
             feature_names = [feature_names]
         template = self.__get_compiled_template(self.DOUBLE_HISTOGRAM_TEMPLATE_NAME)
         if self.reference_profiles:
-            target_profile_columns = json.loads(self.target_profile_jsons[0]).get('columns')
-            reference_profile_columns = json.loads(self.reference_profile_jsons[0]).get('columns')
+            target_profile_columns = json.loads(self.target_profile_jsons[0]).get("columns")
+            reference_profile_columns = json.loads(self.reference_profile_jsons[0]).get("columns")
             target_profile_features, reference_profile_features = {}, {}
             for feature_name in feature_names:
                 target_profile_features[feature_name] = target_profile_columns.get(feature_name)
-                reference_profile_features[feature_name] = reference_profile_columns.get(
-                    feature_name
-                )
-            distribution_chart = template({
-                "profile_from_whylogs": json.dumps(target_profile_features),
-                "reference_profile_from_whylogs": json.dumps(reference_profile_features)
-            })
-            return self.__display_rendered_template(
-                distribution_chart,
-                self.DOUBLE_HISTOGRAM_TEMPLATE_NAME,
-                preferred_cell_height
+                reference_profile_features[feature_name] = reference_profile_columns.get(feature_name)
+            distribution_chart = template(
+                {"profile_from_whylogs": json.dumps(target_profile_features), "reference_profile_from_whylogs": json.dumps(reference_profile_features)}
             )
+            return self.__display_rendered_template(distribution_chart, self.DOUBLE_HISTOGRAM_TEMPLATE_NAME, preferred_cell_height)
         else:
-            logger.warning(
-                "This method has to get both target and reference profiles, with valid feature title"
-            )
+            logger.warning("This method has to get both target and reference profiles, with valid feature title")
             return None
 
-    def feature_summary_statistics(self, feature_name, profile='reference', preferred_cell_height=None):
+    def feature_statistics(self, feature_name, profile="reference", preferred_cell_height=None):
         template = self.__get_compiled_template(self.FEATURE_STATISTICS_TEMPLATE_NAME)
-        if self.reference_profiles and profile.lower() == 'reference':
+        if self.reference_profiles and profile.lower() == "reference":
             selected_profile_json = self.reference_profile_jsons
             selected_profile = self.reference_profiles[0].columns
         else:
             selected_profile_json = self.target_profile_jsons
             selected_profile = self.target_profiles[0].columns
-        rendered_template = template({
-            "profile_feature_summary_statistics_from_whylogs": json.dumps(
-                self.__pull_feature_data(
-                    selected_profile.get(feature_name),
-                    selected_profile_json,
-                    feature_name
+        rendered_template = template(
+            {
+                "profile_feature_statistics_from_whylogs": json.dumps(
+                    self.__pull_feature_data(selected_profile.get(feature_name), selected_profile_json, feature_name)
                 )
-            )}
+            }
         )
-        return self.__display_rendered_template(
-            rendered_template,
-            self.FEATURE_STATISTICS_TEMPLATE_NAME,
-            preferred_cell_height
-        )
+        return self.__display_rendered_template(rendered_template, self.FEATURE_STATISTICS_TEMPLATE_NAME, preferred_cell_height)
 
     def download(self, html, preferred_path=None, html_file_name=None):
         if not html_file_name:
@@ -182,7 +138,7 @@ class NotebookProfileViewer:
         if preferred_path:
             path = os.path.expanduser(preferred_path)
         else:
-            path = os.path.join(os.pardir, "html_reports", str(html_file_name)+".html")
+            path = os.path.join(os.pardir, "html_reports", str(html_file_name) + ".html")
         full_path = os.path.abspath(path)
         with open(full_path, "w") as saved_html:
             saved_html.write(html.data)
@@ -191,11 +147,5 @@ class NotebookProfileViewer:
 
     def constraints_report(self, constraints, preferred_cell_height=None):
         template = self.__get_compiled_template(self.CONSTRAINTS_REPORT_TEMPLATE_NAME)
-        rendered_template = template({
-            "constraints_report": json.dumps(constraints.report())}
-        )
-        return self.__display_rendered_template(
-            rendered_template,
-            self.CONSTRAINTS_REPORT_TEMPLATE_NAME,
-            preferred_cell_height
-        )
+        rendered_template = template({"constraints_report": json.dumps(constraints.report())})
+        return self.__display_rendered_template(rendered_template, self.CONSTRAINTS_REPORT_TEMPLATE_NAME, preferred_cell_height)
--- src/whylogs/viz/utils/profile_viz_calculations.py
+++ src/whylogs/viz/utils/profile_viz_calculations.py
@@ -1,6 +1,9 @@
-from whylogs.core.summaryconverters import ks_test_compute_p_value, compute_chi_squared_test_p_value, single_quantile_from_sketch
-
-from whylogs.proto import ReferenceDistributionDiscreteMessage, InferredType
+from whylogs.core.summaryconverters import (
+    compute_chi_squared_test_p_value,
+    ks_test_compute_p_value,
+    single_quantile_from_sketch,
+)
+from whylogs.proto import InferredType, ReferenceDistributionDiscreteMessage
 
 TYPES = InferredType.Type
 categorical_types = (TYPES.INTEGRAL, TYPES.STRING, TYPES.BOOLEAN)
@@ -39,80 +42,62 @@ def add_drift_val_to_ref_profile_json(target_profile, reference_profile, referen
                 target_kll_sketch = target_col.number_tracker.histogram
                 reference_kll_sketch = ref_col.number_tracker.histogram
                 ks_p_value = ks_test_compute_p_value(target_kll_sketch, reference_kll_sketch)
-                reference_profile_json['columns'][target_col_name]['drift_from_ref'] = ks_p_value.ks_test
+                reference_profile_json["columns"][target_col_name]["drift_from_ref"] = ks_p_value.ks_test
             elif all([type in categorical_types for type in (ref_type, target_type)]) and ref_type == target_type:
                 target_frequent_items_sketch = target_col.frequent_items
                 reference_frequent_items_sketch = ref_col.frequent_items
                 if any([msg.to_summary() is None for msg in (target_frequent_items_sketch, reference_frequent_items_sketch)]):
                     continue
                 target_total_count = target_col.counters.count
-                target_message = ReferenceDistributionDiscreteMessage(
-                    frequent_items=target_frequent_items_sketch.to_summary(),
-                    total_count=target_total_count)
+                target_message = ReferenceDistributionDiscreteMessage(frequent_items=target_frequent_items_sketch.to_summary(), total_count=target_total_count)
                 ref_total_count = ref_col.counters.count
 
                 reference_message = ReferenceDistributionDiscreteMessage(
-                    frequent_items=reference_frequent_items_sketch.to_summary(),
-                    total_count=ref_total_count
+                    frequent_items=reference_frequent_items_sketch.to_summary(), total_count=ref_total_count
                 )
-                chi_squared_p_value = compute_chi_squared_test_p_value(
-                    target_message, reference_message)
+                chi_squared_p_value = compute_chi_squared_test_p_value(target_message, reference_message)
                 if chi_squared_p_value.chi_squared_test:
-                    reference_profile_json['columns'][target_col_name]['drift_from_ref'] = chi_squared_p_value.chi_squared_test
+                    reference_profile_json["columns"][target_col_name]["drift_from_ref"] = chi_squared_p_value.chi_squared_test
                 else:
-                    reference_profile_json['columns'][target_col_name]['drift_from_ref'] = None
-    reference_profile_json['properties']['observations'] = observations
-    reference_profile_json['properties']['missing_cells'] = missing_cells
-    reference_profile_json['properties']['total_count'] = total_count
-    reference_profile_json['properties']['missing_percentage'] = (
-        missing_cells / total_count
-    ) * 100 if total_count else 0
+                    reference_profile_json["columns"][target_col_name]["drift_from_ref"] = None
+    reference_profile_json["properties"]["observations"] = observations
+    reference_profile_json["properties"]["missing_cells"] = missing_cells
+    reference_profile_json["properties"]["total_count"] = total_count
+    reference_profile_json["properties"]["missing_percentage"] = (missing_cells / total_count) * 100 if total_count else 0
 
     return reference_profile_json
 
 
 def calculate_variance(profile_jsons, feature_name):
-    feature = profile_jsons.get(
-        'columns').get(feature_name)
-    variance = feature.get('numberSummary').get(
-        'stddev')**2 if feature.get('numberSummary') != None else 0
+    feature = profile_jsons.get("columns").get(feature_name)
+    variance = feature.get("numberSummary").get("stddev") ** 2 if feature.get("numberSummary") is not None else 0
     return variance
 
 
 def calculate_coefficient_of_variation(profile_jsons, feature_name):
-    feature = profile_jsons.get(
-        'columns').get(feature_name)
-    coefficient_of_variation = feature.get(
-        'numberSummary').get('stddev')/feature.get('numberSummary').get('mean') if feature.get('numberSummary') != None else 0
+    feature = profile_jsons.get("columns").get(feature_name)
+    coefficient_of_variation = (
+        feature.get("numberSummary").get("stddev") / feature.get("numberSummary").get("mean") if feature.get("numberSummary") is not None else 0
+    )
     return coefficient_of_variation
+
+
+def calculate_sum(profile_jsons, feature_name):
+    feature = profile_jsons.get("columns").get(feature_name)
+    sum = feature.get("numberSummary").get("mean") * int(feature.get("counters").get("count"))
+    return sum
 
 
 def calculate_quantile_statistics_for_single_feature(feature, profile_jsons, feature_name):
     quantile_statistics = {}
-    feature_number_summary = profile_jsons.get('columns').get(feature_name).get('numberSummary')
+    feature_number_summary = profile_jsons.get("columns").get(feature_name).get("numberSummary")
     if feature.number_tracker and feature.number_tracker.histogram.get_n() > 0:
         kll_sketch = feature.number_tracker.histogram
-        quantile_statistics['fifth_percentile'] = single_quantile_from_sketch(
-            kll_sketch,
-            quantile=0.05
-        ).quantile
-        quantile_statistics['q1'] = single_quantile_from_sketch(
-            kll_sketch,
-            quantile=0.25
-        ).quantile
-        quantile_statistics['median'] = single_quantile_from_sketch(
-            kll_sketch,
-            quantile=0.5
-        ).quantile
-        quantile_statistics['q3'] = single_quantile_from_sketch(
-            kll_sketch,
-            quantile=0.75
-        ).quantile
-        quantile_statistics['ninety_fifth_percentile'] = single_quantile_from_sketch(
-            kll_sketch,
-            quantile=0.95
-        ).quantile
-        quantile_statistics['range'] = feature_number_summary.get(
-            'max') - feature_number_summary.get('min')
-        quantile_statistics['iqr'] = quantile_statistics['q3'] - quantile_statistics['q1']
+        quantile_statistics["fifth_percentile"] = single_quantile_from_sketch(kll_sketch, quantile=0.05).quantile
+        quantile_statistics["q1"] = single_quantile_from_sketch(kll_sketch, quantile=0.25).quantile
+        quantile_statistics["median"] = single_quantile_from_sketch(kll_sketch, quantile=0.5).quantile
+        quantile_statistics["q3"] = single_quantile_from_sketch(kll_sketch, quantile=0.75).quantile
+        quantile_statistics["ninety_fifth_percentile"] = single_quantile_from_sketch(kll_sketch, quantile=0.95).quantile
+        quantile_statistics["range"] = feature_number_summary.get("max") - feature_number_summary.get("min")
+        quantile_statistics["iqr"] = quantile_statistics["q3"] - quantile_statistics["q1"]
     return quantile_statistics
