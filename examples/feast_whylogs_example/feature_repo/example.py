ccfd62e1aeeef7bf36502a2ba4e006e9400a52e8
Felipe Adachi <felipeadachi@gmail.com>
2022-02-23 16:34:32 -0300

    isort

--- examples/feast_whylogs_example/feature_repo/example.py
+++ examples/feast_whylogs_example/feature_repo/example.py
@@ -1,11 +1,10 @@
 # This is an example feature definition file
 
+from feast import Entity, Feature, FeatureView, FileSource, ValueType
 from google.protobuf.duration_pb2 import Duration
 
-from feast import Entity, Feature, FeatureView, FileSource, ValueType
-
 # Read data from parquet files. Parquet is convenient for local development mode. For
-# production, you can use your favorite DWH, such as BigQuery. See Feast documentation
+# production you can use your favorite DWH, such as BigQuery. See Feast documentation
 # for more info.
 driver_hourly_stats = FileSource(
     path="/work/feature_repo/data/driver_stats.parquet",
@@ -15,7 +14,11 @@ driver_hourly_stats = FileSource(
 
 # Define an entity for the driver. You can think of entity as a primary key used to
 # fetch features.
-driver = Entity(name="driver_id", value_type=ValueType.INT64, description="driver id",)
+driver = Entity(
+    name="driver_id",
+    value_type=ValueType.INT64,
+    description="driver id",
+)
 
 # Our parquet files contain sample data that includes a driver_id column, timestamps and
 # three feature column. Here we define a Feature View that will allow us to serve this
@@ -28,7 +31,6 @@ driver_hourly_stats_view = FeatureView(
         Feature(name="rate_1m", dtype=ValueType.INT64),
         Feature(name="avg_daily_trips", dtype=ValueType.INT64),
         Feature(name="avg_speed", dtype=ValueType.FLOAT),
-
     ],
     online=True,
     batch_source=driver_hourly_stats,
