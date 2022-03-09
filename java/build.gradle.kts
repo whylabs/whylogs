d0a509c0ae6e8da30000089ebe2eededd0978e17
Jamie Broomall <jamie@whylabs.ai>
2022-03-08 16:32:28 -0800

    Bump jar version: 0.1.7-b2 → 0.1.8-b0

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ai.whylabs"
-version = "0.1.7-b2"
+version = "0.1.8-b0"
 //version = "0.1.7-b1-${project.properties.getOrDefault("versionType", "SNAPSHOT")}"
 extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
 
