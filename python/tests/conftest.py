d129ad9daeb586c5bf304e8b17b88f0970060129
Jamie Broomall <jamie@whylabs.ai>
2022-04-08 13:59:42 -0700

    Add test fixture for load tests and add a benchmark test for track_column

---
+++ python/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+def pytest_addoption(parser) -> None:
+    parser.addoption(
+        "--load", action="store_true", default=False, help="run load tests"
+    )
+
+
+def pytest_configure(config) -> None:
+    config.addinivalue_line(
+        "markers", "load: mark test as load to skip running with unit tests"
+    )
+
+
+def pytest_collection_modifyitems(config, items) -> None:
+    if config.getoption("--load"):
+        # --integ specified on command line: do not skip integ tests
+        return
+    skip_load_test = pytest.mark.skip(reason="need --load option to run")
+    for item in items:
+        if "load" in item.keywords:
+            item.add_marker(skip_load_test)
---
+++ python/tests/test_performance.py
@@ -0,0 +1,43 @@
+import cProfile
+import pstats
+from io import StringIO
+from logging import getLogger
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from whylogs_v1.core import ColumnProfile, ColumnSchema
+from whylogs_v1.core.datatypes import NumericColumnType
+
+TEST_LOGGER = getLogger(__name__)
+
+
+@pytest.mark.load
+def test_track_column_benchmark() -> None:
+    row_counts = [1200000]
+    TEST_LOGGER.info("Running log_dataframe_benchmark")
+    for i in range(len(row_counts)):
+        full_df = pd.DataFrame(
+            (np.random.rand(row_counts[i], 1) - 0.5) * 3, columns=["A"]
+        )
+
+        # Create a whylogs-v1 profile
+        string_output_stream = StringIO()
+        profiler = cProfile.Profile()
+        profiler.enable()
+        col_prof = ColumnProfile(ColumnSchema("perf_test", NumericColumnType[int](int)))
+        col_prof.track_column(full_df["A"])
+        profiler.disable()
+        stats = pstats.Stats(profiler, stream=string_output_stream).sort_stats(
+            "cumulative"
+        )
+        stats.print_stats(40)
+        TEST_LOGGER.info(
+            f"stats for dataframe of shape: {full_df.shape} are \n{string_output_stream.getvalue()}"
+        )
+
+        assert col_prof._trackers["hist"] is not None
+        TEST_LOGGER.info(
+            f"Benchmark was using the following trackers {col_prof._trackers.keys()}"
+        )
