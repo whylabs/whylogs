d70c86a33acf14a79bbcabc4b16910f41016cd34
Felipe Adachi <e-felipea@weg.net>
2022-04-18 18:12:04 -0300

    undo change in notebook_tests.py

--- test_notebooks/notebook_tests.py
+++ test_notebooks/notebook_tests.py
@@ -9,7 +9,6 @@ PARENT_DIR = os.path.join(TEST_DIR, os.pardir)
 
 skip_notebooks = [
     "Guest Session Demo.ipynb",
-    "Auto Segmentation.ipynb",
     "RAPIDS GPU Integration Example.ipynb",
     "Analysis.ipynb",
     "ROV-whylogs.ipynb",
