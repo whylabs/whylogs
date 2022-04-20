ecbecea3b5a1e9482a521f0335209e2cb3aa891f
Andy Dang <26821974+andyndang@users.noreply.github.com>
2022-04-19 17:28:22 -0700

    Add v0 protobuf definition into the code base (#25)
    * Add v0 protobuf definition into the code base
    
    * Rename V0 messages with V0
    
    * Fixed preccommit
    
    Co-authored-by: Andy Dang <andy@whylabs.ai>

---
+++ python/whylogs/core/proto/v0/__init__.py
@@ -0,0 +1,8 @@
+"""
+Auto-generated protobuf class definitions.
+
+Protobuf allows us to serialize/deserialize classes across languages
+"""
+from .constraints_pb2 import *  # noqa
+from .messages_pb2 import *  # noqa
+from .summaries_pb2 import *  # noqa
