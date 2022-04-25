f3e9b490b417f4269cc8ffe3d3d1159d87f556b1
Andy Dang <26821974+andyndang@users.noreply.github.com>
2022-04-25 16:09:07 -0700

    Minor code fix (#43)
    Co-authored-by: Andy Dang <andy@whylabs.ai>

--- python/tests/core/view/test_dataset_profile_view.py
+++ python/tests/core/view/test_dataset_profile_view.py
@@ -1,16 +1,19 @@
+import os.path
+
 import pandas as pd
 
 from whylogs.core import DatasetProfile, DatasetProfileView
 
 
-def test_view_serde_roundtriπ() -> None:
+def test_view_serde_roundtrip(tmp_path: str) -> None:
     d = {"col1": [1, 2], "col2": [3.0, 4.0], "col3": ["a", "b"]}
     df = pd.DataFrame(data=d)
 
     profile = DatasetProfile()
     profile.track(pandas=df)
     view = profile.view()
-    view.write("/tmp/data.bin")
-    res = DatasetProfileView.read("/tmp/data.bin")
+    output_file = os.path.join(tmp_path, "view.bin")
+    view.write(output_file)
+    res = DatasetProfileView.read(output_file)
 
     assert len(view.to_pandas()) == len(res.to_pandas())
