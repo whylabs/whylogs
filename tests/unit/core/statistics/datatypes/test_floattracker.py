0ce46f270b90baa690892449bef5eb39b9b62253
TheMellyBee <info@melanieburns.net>
2022-04-25 11:32:31 -0700

    Updates TYPES.FRACTIONAL to include np.float32 and np.float64 and adds tests in the FloatTracker

--- tests/unit/core/statistics/datatypes/test_floattracker.py
+++ tests/unit/core/statistics/datatypes/test_floattracker.py
@@ -1,3 +1,4 @@
+import numpy as np
 from whylogs.core.statistics.datatypes import FloatTracker
 
 
@@ -11,6 +12,28 @@ def test_values_are_min_max():
     assert first.max == max(vals1)
     assert first.min == min(vals1)
     assert first.sum == sum(vals1)
+
+
+def test_np_float():
+    first = FloatTracker()
+    float32_array = np.array([1.0, 2.0, 3.0], dtype=np.float32)
+    for val in float32_array:
+        first.update(val)
+
+    assert first.count == len(float32_array)
+    assert first.max == max(float32_array)
+    assert first.min == min(float32_array)
+    assert first.sum == sum(float32_array)
+
+    second = FloatTracker()
+    float64_array = np.array([1.0, 2.0, 3.0], dtype=np.float64)
+    for val in float64_array:
+        second.update(val)
+
+    assert second.count == len(float64_array)
+    assert second.max == max(float64_array)
+    assert second.min == min(float64_array)
+    assert second.sum == sum(float64_array)
 
 
 def test_merge_floattrackers_should_addup():
