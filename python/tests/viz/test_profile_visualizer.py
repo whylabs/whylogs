2ce04926022bb02041e63901ad2ade157e6973ea
Felipe Adachi <felipeadachi@gmail.com>
2022-04-29 17:18:38 -0300

    [WIP] add difference_distribution_chart (#57)

--- python/tests/viz/test_profile_visualizer.py
+++ python/tests/viz/test_profile_visualizer.py
@@ -120,3 +120,26 @@ def test_viz_distribution_chart_two_profiles(tmp_path: str) -> None:
         html_file_name=test_output,
     )
     webbrowser.open(f"file://{os.path.realpath(test_output)}.html", new=2)
+
+
+def test_viz_difference_distribution_chart_two_profiles(tmp_path: str) -> None:
+    data = {
+        "animal": ["cat", "hawk", "snake", "cat"],
+        "legs": [4, 2, 0, 4],
+        "weight": [4.3, 1.8, None, 4.1],
+    }
+
+    df = pd.DataFrame(data)
+
+    results = why.log(pandas=df)
+    profile_view = results.view()
+
+    visualization = NotebookProfileVisualizer()
+    visualization.set_profiles(target_profile_view=profile_view, reference_profile_view=profile_view)
+
+    test_output = os.path.join(tmp_path, "b18")
+    visualization.write(
+        rendered_html=visualization.difference_distribution_chart(feature_name="animal"),
+        html_file_name=test_output,
+    )
+    webbrowser.open(f"file://{os.path.realpath(test_output)}.html", new=2)
