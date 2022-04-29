3ecab47be54d58665f7021a48cefdc6fa8c1fcea
murilommen <murilommen@gmail.com>
2022-04-29 11:21:57 -0300

    refact viz tests

--- python/tests/viz/test_profile_visualizer.py
+++ python/tests/viz/test_profile_visualizer.py
@@ -1,25 +1,18 @@
 import os
 import webbrowser
 
-import pandas as pd
+import pytest
 
-import whylogs as why
 from whylogs.viz import NotebookProfileVisualizer
 
 
-def test_viz_feature_statistics(tmp_path: str) -> None:
-    data = {
-        "animal": ["cat", "hawk", "snake", "cat"],
-        "legs": [4, 2, 0, 4],
-        "weight": [4.3, 1.8, None, 4.1],
-    }
-
-    df = pd.DataFrame(data)
-
-    results = why.log(pandas=df)
-    profile_view = results.view()
-
+@pytest.fixture(scope="session")
+def visualization() -> NotebookProfileVisualizer:
     visualization = NotebookProfileVisualizer()
+    return visualization
+
+
+def test_viz_feature_statistics(profile_view, visualization, tmp_path: str) -> None:
     visualization.set_profiles(target_profile_view=profile_view, reference_profile_view=profile_view)
 
     test_output = os.path.join(tmp_path, "b18")
@@ -30,19 +23,7 @@ def test_viz_feature_statistics(tmp_path: str) -> None:
     webbrowser.open(f"file://{os.path.realpath(test_output)}.html", new=2)
 
 
-def test_viz_double_histogram_single_profile(tmp_path: str) -> None:
-    data = {
-        "animal": ["cat", "hawk", "snake", "cat"],
-        "legs": [4, 2, 0, 4],
-        "weight": [4.3, 1.8, None, 4.1],
-    }
-
-    df = pd.DataFrame(data)
-
-    results = why.log(pandas=df)
-    profile_view = results.view()
-
-    visualization = NotebookProfileVisualizer()
+def test_viz_double_histogram_single_profile(profile_view, visualization, tmp_path: str) -> None:
     visualization.set_profiles(target_profile_view=profile_view)
 
     test_output = os.path.join(tmp_path, "b18")
@@ -53,19 +34,7 @@ def test_viz_double_histogram_single_profile(tmp_path: str) -> None:
     webbrowser.open(f"file://{os.path.realpath(test_output)}.html", new=2)
 
 
-def test_viz_double_histogram_two_profiles(tmp_path: str) -> None:
-    data = {
-        "animal": ["cat", "hawk", "snake", "cat"],
-        "legs": [4, 2, 0, 4],
-        "weight": [4.3, 1.8, None, 4.1],
-    }
-
-    df = pd.DataFrame(data)
-
-    results = why.log(pandas=df)
-    profile_view = results.view()
-
-    visualization = NotebookProfileVisualizer()
+def test_viz_double_histogram_two_profiles(profile_view, visualization, tmp_path: str) -> None:
     visualization.set_profiles(target_profile_view=profile_view, reference_profile_view=profile_view)
 
     test_output = os.path.join(tmp_path, "b18")
@@ -76,19 +45,7 @@ def test_viz_double_histogram_two_profiles(tmp_path: str) -> None:
     webbrowser.open(f"file://{os.path.realpath(test_output)}.html", new=2)
 
 
-def test_viz_distribution_chart_single_profile(tmp_path: str) -> None:
-    data = {
-        "animal": ["cat", "hawk", "snake", "cat"],
-        "legs": [4, 2, 0, 4],
-        "weight": [4.3, 1.8, None, 4.1],
-    }
-
-    df = pd.DataFrame(data)
-
-    results = why.log(pandas=df)
-    profile_view = results.view()
-
-    visualization = NotebookProfileVisualizer()
+def test_viz_distribution_chart_single_profile(profile_view, visualization, tmp_path: str) -> None:
     visualization.set_profiles(target_profile_view=profile_view)
 
     test_output = os.path.join(tmp_path, "b18")
@@ -99,19 +56,7 @@ def test_viz_distribution_chart_single_profile(tmp_path: str) -> None:
     webbrowser.open(f"file://{os.path.realpath(test_output)}.html", new=2)
 
 
-def test_viz_distribution_chart_two_profiles(tmp_path: str) -> None:
-    data = {
-        "animal": ["cat", "hawk", "snake", "cat"],
-        "legs": [4, 2, 0, 4],
-        "weight": [4.3, 1.8, None, 4.1],
-    }
-
-    df = pd.DataFrame(data)
-
-    results = why.log(pandas=df)
-    profile_view = results.view()
-
-    visualization = NotebookProfileVisualizer()
+def test_viz_distribution_chart_two_profiles(profile_view, visualization, tmp_path: str) -> None:
     visualization.set_profiles(target_profile_view=profile_view, reference_profile_view=profile_view)
 
     test_output = os.path.join(tmp_path, "b18")
