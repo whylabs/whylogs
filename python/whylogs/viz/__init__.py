a8eb7ed2fa5f4fd6609decece8563ac7722afa54
Andy Dang <andy@whylabs.ai>
2022-05-02 16:38:14 -0700

    Make IPython import more stable

--- python/whylogs/viz/__init__.py
+++ python/whylogs/viz/__init__.py
@@ -1,4 +1,6 @@
-from .jupyter_notebook_viz import NotebookProfileVisualizer
+import IPython  # type: ignore  # noqa  # import ipython to make
+
+from whylogs.viz.jupyter_notebook_viz import NotebookProfileVisualizer
 
 __ALL__ = [
     # column
