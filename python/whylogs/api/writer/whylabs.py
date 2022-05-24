161cad385a750c1db54e1147d6dae14ffac97c8a
murilommen <murilommen@gmail.com>
2022-05-24 18:01:24 -0300

    adding env var check + docstrings

--- python/whylogs/api/writer/whylabs.py
+++ python/whylogs/api/writer/whylabs.py
@@ -19,11 +19,51 @@ logger = logging.getLogger(__name__)
 
 
 class WhyLabsWriter(Writer):
+    """
+    A WhyLogs writer to upload DatasetProfileView's onto the WhyLabs platform.
+
+    >**IMPORTANT**: In order to correctly send your profiles over, make sure you have
+    the following environment variables set: `[WHYLABS_ORG_ID, WHYLABS_API_KEY]`. You
+    can also set them with the option method or within the constructor, although it
+    is highly recommended you don't persist credentials in code!
+
+    Parameters
+    ----------
+    org_id: str, optional
+        Your organization ID on the WhyLabs platform. If not informed, will get the mentioned environment
+        variable instead.
+
+    api_key: str, optional
+        The API Key to authenticate with WhyLabs. If not informed, will get the mentioned environment
+        variable instead.
+
+    dataset_id: str, optional
+        The Dataset ID, often referred to as model ID, that will tie the profile to your tracked data.
+        If not informed, will get the mentioned environment variable instead.
+    Returns
+    -------
+        None
+
+    Examples
+    --------
+    An example usage of this method can be represented with the simple code above. Here we
+    are assuming the user has already assigned a pandas.DataFrame to the `df` variable.
+
+    ```python
+    import whylogs as why
+
+    profile = why.log(pandas=df)
+    profile.writer("whylabs").write()
+    ```
+
+    """
+
     def __init__(self, org_id: Optional[str] = None, api_key: Optional[str] = None, dataset_id: Optional[str] = None):
         self._org_id = org_id or os.environ.get("WHYLABS_ORG_ID")
         self._api_key = api_key or os.environ.get("WHYLABS_API_KEY")
         self._dataset_id = dataset_id or os.environ.get("WHYLABS_DEFAULT_DATASET_ID")
         self.whylabs_api_endpoint = os.environ.get("WHYLABS_API_ENDPOINT") or "https://api.whylabsapp.com"
+        self._whylabs_enabled = os.environ.get("WHYLABS_V1_ENABLED")
 
     def check_interval(self, interval_seconds: int):
         if interval_seconds < FIVE_MINUTES_IN_SECONDS:
@@ -40,6 +80,9 @@ class WhyLabsWriter(Writer):
             self._api_key = api_key
 
     def write(self, profile: DatasetProfileView, dataset_id: Optional[str] = None) -> Any:
+        if self._whylabs_v1_enabled != "True":
+            raise ValueError("The Whylabs writer is currently not supported!")
+
         if dataset_id is not None:
             self._dataset_id = dataset_id
 
