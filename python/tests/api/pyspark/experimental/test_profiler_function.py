90c49ca1703df9cce75a657aca1cb2f7a96f1d2d
Murilo Menezes Mendonça <murilommen@gmail.com>
2022-05-25 19:59:28 -0300

    adding pyspark example (#576)

--- python/tests/api/pyspark/experimental/test_profiler_function.py
+++ python/tests/api/pyspark/experimental/test_profiler_function.py
@@ -1,42 +1,47 @@
-import unittest
 from logging import getLogger
 from typing import Dict
 
+import pytest
 from pyspark.sql import SparkSession
 
 from whylogs.api.pyspark.experimental import (
     collect_column_profile_views,
+    collect_dataset_profile_view,
     column_profile_bytes_aggregator,
     whylogs_pandas_map_profiler,
 )
 from whylogs.core import ColumnProfileView
+from whylogs.core.view.dataset_profile_view import DatasetProfileView
 
 TEST_LOGGER = getLogger(__name__)
 
 
-class PysparkTest(unittest.TestCase):
-    """Set-up a test SparkSession"""
-
+class TestPySpark(object):
     @classmethod
-    def setUpClass(cls):
+    def setup_class(cls):
         cls.spark = SparkSession.builder.master("local[1]").getOrCreate()
 
     @classmethod
-    def tearDownClass(cls):
+    def teardown_class(cls):
         cls.spark.stop()
 
+    @pytest.fixture()
+    def test_columns(self):
+        return ["0", "1", "2", "3"]
 
-class WhysparkTests(PysparkTest):
-    def test_profile_mapper_function(self):
+    @pytest.fixture()
+    def input_df(self, test_columns):
         input_df = self.spark.createDataFrame(
             data=[
                 [0.0, 1.0, 2.0, 3.0],
                 [0.1, 1.1, 2.1, 3.1],
                 [0.2, 1.3, 2.3, 3.3],
             ],
-            schema=["0", "1", "2", "3"],
+            schema=test_columns,
         )
+        return input_df
 
+    def test_profile_mapper_function(self, input_df):
         # TODO: consider a constant for this, or better encapsulation.
         cp = "col_name string, col_profile binary"
 
@@ -53,17 +58,7 @@ class WhysparkTests(PysparkTest):
         TEST_LOGGER.info(local_column_profiles)
         assert local_column_profiles is not None
 
-    def test_collect_column_profiles(self):
-        test_columns = ["0", "1", "2", "3"]
-        input_df = self.spark.createDataFrame(
-            data=[
-                [0.0, 1.0, 2.0, 3.0],
-                [0.1, 1.1, 2.1, 3.1],
-                [0.2, 1.3, 2.3, 3.2],
-            ],
-            schema=test_columns,
-        )
-
+    def test_collect_column_profiles(self, input_df, test_columns):
         # test pyspark whylogs profiling
         local_column_profiles = collect_column_profile_views(input_df)
         TEST_LOGGER.info(local_column_profiles)
@@ -76,3 +71,12 @@ class WhysparkTests(PysparkTest):
         assert distribution_metric.stddev == 0.1
         assert distribution_metric.max == 0.2
         assert distribution_metric.min == 0.0
+
+    def test_collect_dataset_profile_view(self, input_df):
+        profile_view = collect_dataset_profile_view(input_df=input_df)
+
+        assert isinstance(profile_view, DatasetProfileView)
+        assert len(profile_view.get_columns()) > 0
+        assert profile_view.get_column("0").get_metric("counts").n.value == 3
+        assert profile_view.get_column("0").get_metric("distribution").max == 0.2
+        assert profile_view.get_column("0").get_metric("distribution").min == 0.0
