8f681f4c4d2dceb5d4a7219a109c839d95dcddea
murilommen <murilommen@gmail.com>
2022-05-25 19:03:57 -0300

    adding pyspark example

--- python/whylogs/api/pyspark/experimental/__init__.py
+++ python/whylogs/api/pyspark/experimental/__init__.py
@@ -1,5 +1,6 @@
 from .profiler import (
     collect_column_profile_views,
+    collect_dataset_profile_view,
     column_profile_bytes_aggregator,
     whylogs_pandas_map_profiler,
 )
@@ -8,4 +9,5 @@ __ALL__ = [
     whylogs_pandas_map_profiler,
     column_profile_bytes_aggregator,
     collect_column_profile_views,
+    collect_dataset_profile_view,
 ]
