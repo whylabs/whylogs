c1cce8ed52cfcbcb8a57e60d79b47b9b91e8f49d
Felipe Adachi <felipeadachi@gmail.com>
2022-06-28 15:03:51 -0300

    added each notebooks as pytest scenarios

--- python/test_notebooks/notebook_tests.py
+++ python/test_notebooks/notebook_tests.py
@@ -8,8 +8,19 @@ TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(TEST_DIR, os.pardir, os.pardir)
 skip_notebooks = ["Mlflow_Logging.ipynb", "Pyspark_Profiling.ipynb", "Writing_Profiles.ipynb", "Kafka_Example.ipynb"]
 
+# https://docs.pytest.org/en/6.2.x/example/parametrize.html#a-quick-port-of-testscenarios
+def pytest_generate_tests(metafunc):
+    idlist = []
+    argvalues = []
+    for scenario in metafunc.cls.scenarios:
+        idlist.append(scenario[0])
+        items = scenario[1].items()
+        argnames = [x[0] for x in items]
+        argvalues.append([x[1] for x in items])
+    metafunc.parametrize(argnames, argvalues, ids=idlist, scope="class")
 
-def process_notebook(notebook_filename, html_directory="notebook-html"):
+
+def process_notebook(notebook_filename):
     """
     Checks if an IPython notebook runs without error from start to finish. If so, writes the
     notebook to HTML (with outputs) and overwrites the .ipynb file (without outputs).
@@ -31,26 +42,14 @@ def process_notebook(notebook_filename, html_directory="notebook-html"):
     return
 
 
-def test_all_notebooks(remove_fail_test=True):
-    """
-    Runs `process_notebook` on all notebooks in the git repository.
-    """
-
-    # Get all files included in the git repository
+class TestNotebooks:
     git_files = (
         subprocess.check_output("git ls-tree --full-tree --name-only -r HEAD", shell=True).decode("utf-8").splitlines()
     )
 
     # Get just the notebooks from the git files
     notebooks = [fn for fn in git_files if fn.endswith(".ipynb") and os.path.basename(fn) not in skip_notebooks]
-    print(notebooks)
-    # Test each notebook
-    for notebook in notebooks:
-        print("Testing", notebook)
+    scenarios = [(notebook, {"notebook": notebook}) for notebook in notebooks]
+
+    def test_all_notebooks(self, notebook):
         process_notebook(os.path.join(PARENT_DIR, notebook))
-
-    return
-
-
-# if __name__ == "__main__":
-#     test_all_notebooks()
