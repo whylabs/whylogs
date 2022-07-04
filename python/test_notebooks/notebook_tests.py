1126877aa29b526460e5e371253eb003db2565f9
Felipe Adachi <felipeadachi@gmail.com>
2022-07-04 16:08:53 -0300

    remove mlflow logging from skip list

--- python/test_notebooks/notebook_tests.py
+++ python/test_notebooks/notebook_tests.py
@@ -6,7 +6,7 @@ from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(TEST_DIR, os.pardir, os.pardir)
-skip_notebooks = ["Mlflow_Logging.ipynb", "Pyspark_Profiling.ipynb", "Kafka_Example.ipynb"]
+skip_notebooks = ["Pyspark_Profiling.ipynb", "Kafka_Example.ipynb"]
 
 
 # https://docs.pytest.org/en/6.2.x/example/parametrize.html#a-quick-port-of-testscenarios
