e01d0a4518e6a84e08a55eba4ec154f3753be702
TheMellyBee <info@melanieburns.net>
2022-07-07 10:23:03 -0700

    Basic port-over, flask is working

---
+++ python/examples/integrations/flask_streaming/settings.py
@@ -0,0 +1,12 @@
+import os
+
+ENV = os.environ.get("FLASK_ENV", "development")
+DEBUG = os.environ.get("FLASK_DEBUG", 0)
+SWAGGER_HOST = os.environ.get("SWAGGER_HOST", None)
+SWAGGER_BASEPATH = os.environ.get("SWAGGER_BASEPATH", None)
+SWAGGER_SCHEMES = os.environ.get("SWAGGER_SCHEMES", None)
+DATASET_URL = os.environ.get("DATASET_URL", None)
+ROTATION_TIME = os.environ.get("ROTATION_TIME", None)
+
+why_log = None
+df = None
