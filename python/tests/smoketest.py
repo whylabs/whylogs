36c4a6861180458057a76c8150a2a3d29c663359
TheMellyBee <info@melanieburns.net>
2022-07-13 16:54:35 -0700

    bumps version from 1.0.5 to 1.0.6

--- python/tests/smoketest.py
+++ python/tests/smoketest.py
@@ -12,7 +12,7 @@ intended to test the wheel in a production environment,
 not a development environment.
 """
 
-current_version = "1.0.5"
+current_version = "1.0.6"
 
 
 def test_package_version() -> None:
