b071614a2391d3419e30add09d0558ae91866498
murilommen <murilommen@gmail.com>
2022-07-20 18:06:45 -0300

    stabilizing IPython init
    details: tests are intermittently passing, due to a circular import in IPython. moved SummaryDriftReport to whylogs.viz init to try to avoid this

--- python/whylogs/viz/__init__.py
+++ python/whylogs/viz/__init__.py
@@ -1,8 +1,10 @@
 import IPython  # type: ignore  # noqa  # import ipython to make
 
+from whylogs.viz.extensions.reports.summary_drift import SummaryDriftReport
 from whylogs.viz.notebook_profile_viz import NotebookProfileVisualizer
 
 __ALL__ = [
     # column
     NotebookProfileVisualizer,
+    SummaryDriftReport,
 ]
