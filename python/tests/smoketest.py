de0198c0bca79db712533e0ef7b3d7dd910697d2
Jamie Broomall <88007022+jamie256@users.noreply.github.com>
2022-08-03 07:33:18 -0700

    Release 1.0.9 (#723)
    * Update version to 1.0.9
    * Update patterns in bumpversion.cfg for new structure in v1

--- python/tests/smoketest.py
+++ python/tests/smoketest.py
@@ -12,7 +12,7 @@ intended to test the wheel in a production environment,
 not a development environment.
 """
 
-current_version = "1.0.8"
+current_version = "1.0.9"
 
 
 def test_package_version() -> None:
