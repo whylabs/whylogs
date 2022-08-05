289bc9a44fe8832a34e20349181805f78791f8ac
TheMellyBee <info@melanieburns.net>
2022-08-04 18:06:48 -0700

    Adds the errors from v1

---
+++ java/core/src/main/java/com/whylogs/core/errors/UnsupportedError.java
@@ -0,0 +1,10 @@
+package com.whylogs.core.errors;
+
+/***
+ * Exception raised when an operation is not supported.
+ */
+public class UnsupportedError extends Error{
+    public UnsupportedError(String message) {
+        super(message);
+    }
+}
