743e907f93127d3fbe9456979f21d51d3a23d699
Jamie Broomall <jamie@whylabs.ai>
2022-08-09 11:09:12 -0700

    Bump version: 1.0.9 → 1.0.10

--- python/tests/smoketest.py
+++ python/tests/smoketest.py
@@ -12,7 +12,7 @@ intended to test the wheel in a production environment,
 not a development environment.
 """
 
-current_version = "1.0.9"
+current_version = "1.0.10"
 
 
 def test_package_version() -> None:
