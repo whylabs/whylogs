5d512bad9609c3bfac3bbd01f12dbd8a183b1ee9
murilommen <murilommen@gmail.com>
2022-08-11 14:55:22 -0300

    adding docstrings to s3 reader

--- python/whylogs/api/reader/s3.py
+++ python/whylogs/api/reader/s3.py
@@ -3,17 +3,43 @@ from typing import Optional
 
 import boto3
 
+from whylogs import ResultSet
 from whylogs.api.reader.reader import Reader
-from whylogs.core import DatasetProfile
 
 
 class S3Reader(Reader):
+    """
+    A WhyLogs reader to fetch written profiles from an Amazon S3 bucket.
+
+    >**IMPORTANT**: In order to correctly connect to your Amazon S3 bucket, make sure you have
+    the following environment variables set: `[AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY]`.
+
+    Parameters
+    ----------
+    bucket_name: str, optional
+        The name of the bucket to connect to. Made optional so the user can also access it
+        via the `option` method
+    object_name: str, optional
+        The s3's object name. It basically states the location where the file goes to.
+        Also made optional, so it can be defined through the `option` method
+
+    Examples
+    --------
+    An example usage of this method can be represented with the simple code below.
+
+    ```python
+    import whylogs as why
+
+    profile = why.reader("s3").option(bucket_name="my_bucket", object_name="my/object.bin").read()
+    ```
+    """
+
     def __init__(self, object_name: Optional[str] = None, bucket_name: Optional[str] = None):
         self.s3_client = boto3.client("s3")
         self.object_name = object_name or None
         self.bucket_name = bucket_name or ""
 
-    def read(self, **kwargs) -> DatasetProfile:
+    def read(self, **kwargs) -> ResultSet:
         with NamedTemporaryFile() as tmp_file:
             self.s3_client.download_file(Bucket=self.bucket_name, Key=self.object_name, Filename=tmp_file.name)
             tmp_file.flush()
