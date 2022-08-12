a468385d5ad9e131c6b991b543634f02dd5763db
Richard Rogers <richard@whylabs.ai>
2022-08-12 14:04:35 -0700

    pre-commit

--- python/tests/core/test_schema.py
+++ python/tests/core/test_schema.py
@@ -1,33 +1,38 @@
-import numpy as np
-import pandas as pd
-import pytest
 from dataclasses import dataclass, field
 from typing import Any, Dict
-from whylogs.core import DatasetSchema, DatasetProfile
+
+import numpy as np
+import pandas as pd
+
+from whylogs.core import DatasetSchema
 from whylogs.core.resolvers import Resolver, StandardResolver
 
+
 class MyResolver(StandardResolver):
-   pass
+    pass
+
 
 @dataclass
 class MySchema(DatasetSchema):
-    types: Dict[str, Any] = field(default_factory=lambda: {
-        "col1": str,
-        "col2": np.int32,
-        "col3": pd.CategoricalDtype(categories=('foo', 'bar'), ordered=True)
-    })
+    types: Dict[str, Any] = field(
+        default_factory=lambda: {
+            "col1": str,
+            "col2": np.int32,
+            "col3": pd.CategoricalDtype(categories=("foo", "bar"), ordered=True),
+        }
+    )
     resolvers: Resolver = field(default_factory=MyResolver)
     cache_size: int = 12
 
 
 def test_schema_default_value_overrides() -> None:
-   schema = MySchema()
-   assert isinstance(schema.resolvers, MyResolver)
-   assert schema.types["col2"] == np.int32
-   assert schema.cache_size == 12
+    schema = MySchema()
+    assert isinstance(schema.resolvers, MyResolver)
+    assert schema.types["col2"] == np.int32
+    assert schema.cache_size == 12
 
 
 def test_schema_subclass_copy() -> None:
-   schema = MySchema()
-   copy = schema.copy()
-   assert isinstance(copy, MySchema)
+    schema = MySchema()
+    copy = schema.copy()
+    assert isinstance(copy, MySchema)
