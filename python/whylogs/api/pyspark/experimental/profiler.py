a1029a7344c0ef162e964f3b6630cd1b5e8a4f43
Andy Dang <andy@whylabs.ai>
2022-08-18 11:22:32 -0700

    Using our dedicated stats endpoint for telemetry collection

--- python/whylogs/api/pyspark/experimental/profiler.py
+++ python/whylogs/api/pyspark/experimental/profiler.py
@@ -7,8 +7,10 @@ import whylogs as why
 from whylogs.core.stubs import pd
 from whylogs.core.view.column_profile_view import ColumnProfileView
 from whylogs.core.view.dataset_profile_view import DatasetProfileView
+from whylogs.api.usage_stats import emit_usage
 
 logger = getLogger(__name__)
+emit_usage("pyspark")
 
 try:  # type: ignore
     from pyspark.sql import DataFrame as SparkDataFrame  # types: ignore
