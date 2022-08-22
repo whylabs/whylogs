dc56777c7fc6adadcdc4464fbb16e12a318856ac
TheMellyBee <info@melanieburns.net>
2022-08-22 16:16:53 -0700

    Running spotless

--- java/core/src/main/java/com/whylogs/core/errors/SerializationError.java
+++ java/core/src/main/java/com/whylogs/core/errors/SerializationError.java
@@ -1,10 +1,8 @@
 package com.whylogs.core.errors;
 
-/**
- * Exception raised when serializing data.
- */
-public class SerializationError extends Error{
-    public SerializationError(String message) {
-        super(message);
-    }
+/** Exception raised when serializing data. */
+public class SerializationError extends Error {
+  public SerializationError(String message) {
+    super(message);
+  }
 }
