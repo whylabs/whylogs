d2fdb65fd54e970cd93267c5aadd40a885c32f9f
Andy Dang <andy@whylabs.ai>
2022-08-24 16:17:36 -0700

    Bump2version to 1.0.12-dev0

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -8,7 +8,7 @@ if shutil.which("pandoc") is None:
     print("Pandoc is required to build our documentation.")
     sys.exit(1)
 
-version = "1.0.11"
+version = "1.0.12-dev0"
 
 project = "whylogs"
 author = "whylogs developers"
