65340f6507f4758e089a8ce4093f4f7f37c99998
TheMellyBee <info@melanieburns.net>
2022-08-26 12:45:23 -0700

    Code and testing for StandardMetric (note this is a factory were we will need to add the rest of the metrics that are currently commented out after ints are done)

---
+++ java/core/src/main/java/com/whylogs/core/metrics/StandardMetric.java
@@ -0,0 +1,51 @@
+package com.whylogs.core.metrics;
+
+public enum StandardMetric {
+    /*
+    types {
+
+    },
+    distribution{
+
+    },
+    counts{
+
+    },*/
+    ints {
+        @Override
+        public IntegralMetric zero(MetricConfig config) {
+            return IntegralMetric.zero(config);
+        }
+    },
+    /*
+    cardinality {
+
+    },
+    frequent_items {
+
+    },
+    unicode_range {
+
+    },
+    condition_count{
+
+    }*/
+    ;
+    abstract <T extends Metric> T zero(MetricConfig config);
+
+    public static <T extends Metric> T getMetric(String name){
+        return StandardMetric.valueOf(name).zero(new MetricConfig());
+    }
+
+    public static <T extends Metric> T getMetric(String name, MetricConfig config){
+        return StandardMetric.valueOf(name).zero(config);
+    }
+
+    public static <T extends Metric> T getMetric(StandardMetric metric){
+        return metric.zero(new MetricConfig());
+    }
+
+    public static <T extends Metric> T getMetric(StandardMetric metric, MetricConfig config){
+        return metric.zero(config);
+    }
+}
--- java/core/src/test/java/com/whylogs/core/metrics/TestStandardMetricFactory.java
+++ java/core/src/test/java/com/whylogs/core/metrics/TestStandardMetricFactory.java
@@ -3,7 +3,6 @@ package com.whylogs.core.metrics;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 @Test
@@ -11,13 +10,12 @@ public class TestStandardMetricFactory {
 
     @Test
     public void test_standardMetric(){
-        IntegralMetric ints = StandardMetricFactory.ints.zero(new MetricConfig());
+        IntegralMetric ints = StandardMetric.ints.zero(new MetricConfig());
         Assert.assertEquals((int) ints.getMaxComponent().getValue(), Integer.MIN_VALUE);
 
         ArrayList<Metric> list = new ArrayList<>();
         list.add(ints);
-        list.add(StandardMetricFactory.ints.zero(new MetricConfig()));
-        Assert.assertEquals(list.size(), 2);
+        list.add(StandardMetric.ints.zero(new MetricConfig()));
     }
 
 }
