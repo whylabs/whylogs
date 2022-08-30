4e9cf91c61735d56655d141db9ef497ca24267cd
TheMellyBee <info@melanieburns.net>
2022-08-30 14:40:49 -0700

    Adding in privacy and an lombrok constructor instead

--- java/core/src/main/java/com/whylogs/core/schemas/ColumnSchema.java
+++ java/core/src/main/java/com/whylogs/core/schemas/ColumnSchema.java
@@ -5,22 +5,18 @@ import com.whylogs.core.metrics.MetricConfig;
 import com.whylogs.core.resolvers.Resolver;
 import java.lang.reflect.Type;
 import java.util.HashMap;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
+@Data @AllArgsConstructor
 public class ColumnSchema {
   // Thoughts: we could have this ColumnSchema<T> instead of having it as a member
   // bu this might be easier to use? If we did we would need to use the CRTP again
   // like in Metric to be able to see the type but also have them in a collection togehter
-  Type type;
-  MetricConfig config;
-  Resolver resolver;
-
-  public ColumnSchema(Type type, MetricConfig config, Resolver resolver) {
-    this.type = type;
-    this.config = config;
-    this.resolver = resolver;
-  }
+  private Type type;
+  private MetricConfig config;
+  private Resolver resolver;
 
   public <T extends Metric> HashMap<String, T> getMetrics() {
     return this.resolver.resolve(this);
