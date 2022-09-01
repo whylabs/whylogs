96f02328f79d264d53534252d80ee13af9e27d56
TheMellyBee <info@melanieburns.net>
2022-09-01 14:17:53 -0700

    Refactor to not use the StandardMetric.java partial factory. Allow resolver to directly call zero() as it builds instead of reflection or through another point

--- java/core/src/main/java/com/whylogs/core/resolvers/StandardResolver.java
+++ java/core/src/main/java/com/whylogs/core/resolvers/StandardResolver.java
@@ -1,10 +1,11 @@
 package com.whylogs.core.resolvers;
 
 import com.whylogs.core.DataTypes;
+import com.whylogs.core.metrics.IntegralMetric;
 import com.whylogs.core.metrics.Metric;
-import com.whylogs.core.metrics.StandardMetric;
 import com.whylogs.core.schemas.ColumnSchema;
-import java.util.ArrayList;
+import org.apache.commons.lang3.NotImplementedException;
+
 import java.util.HashMap;
 
 public class StandardResolver extends Resolver {
@@ -13,40 +14,23 @@ public class StandardResolver extends Resolver {
   }
 
   // TODO: the rest of the metrics need implmeented
-  // TODO: does this Metric loose it's typing?
   @Override
   public HashMap<String, Metric> resolve(ColumnSchema schema) {
-    ArrayList<StandardMetric> standardMetrics = new ArrayList<>();
-    // standardMetrics.add(StandardMetric.counts, StandardMetric.types)
+    HashMap<String, Metric> resolvedMetrics = new HashMap<>();
 
     if (DataTypes.Integral.includes(schema.getType())) {
-      standardMetrics.add(StandardMetric.ints);
-      // standardMetrics.add(StandardMetric.distribution);
-      // standardMetrics.add(StandardMetric.cardinality);
-      // standardMetrics.add(StandardMetric.frequent_items);
+      resolvedMetrics.put(IntegralMetric.NAMESPACE, IntegralMetric.zero(schema.getConfig()));
     } else if (DataTypes.Fractional.includes(schema.getType())) {
-      // standardMetrics.add(StandardMetric.distribution);
-      // standardMetrics.add(StandardMetric.cardinality);
+      throw new NotImplementedException("Fractional metrics not implemented");
     } else if (DataTypes.String.includes(schema.getType())) {
-      // standardMetrics.add(StandardMetric.cardinality);
-      // standardMetrics.add(StandardMetric.distribution);
-      // standardMetrics.add(StandardMetric.frequent_items);
-
       if (schema.getConfig().isTrack_unicode_ranges()) {
-        // standardMetrics.add(StandardMetric.unicode_range);
+        throw new NotImplementedException("String & Unicode metrics not implemented");
       }
     }
 
     if (schema.getConfig().isFi_disabled()) {
-      // standardMetrics.remove(StandardMetric.frequent_items);
+      throw new NotImplementedException("Frequent Items metrics not implemented");
     }
-
-    HashMap<String, Metric> result = new HashMap<>();
-
-    for (StandardMetric metric : standardMetrics) {
-      result.put(metric.name(), StandardMetric.getMetric(metric, schema.getConfig()));
-    }
-
-    return result;
+    return resolvedMetrics;
   }
 }
