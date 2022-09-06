d96eb2b152be760296889e13ae0ca590adbdb76c
Jamie Broomall <jamie@whylabs.ai>
2022-09-06 10:02:50 -0700

    Update version info to 0.7.10

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,7 @@ copyright = "2020, WhyLabs, Inc"
 # built documents.
 #
 # The short X.Y version.
-version = "0.7.9"
+version = "0.7.10-dev0"
 # The full version, including alpha/beta/rc tags.
 release = ""  # Is set by calling `setup.py docs`
 
--- src/whylogs/_version.py
+++ src/whylogs/_version.py
@@ -1,3 +1,3 @@
 """WhyLabs version number."""
 
-__version__ = "0.7.9"
+__version__ = "0.7.10"
