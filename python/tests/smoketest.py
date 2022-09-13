0f4c36e78468156866f814b1ac3b723114871b4d
Jamie Broomall <jamie@whylabs.ai>
2022-09-13 13:17:35 -0700

    Update version to 1.1.0rc2

--- python/tests/smoketest.py
+++ python/tests/smoketest.py
@@ -12,7 +12,7 @@ intended to test the wheel in a production environment,
 not a development environment.
 """
 
-current_version = "1.0.12"
+current_version = "1.1.0rc2"
 
 
 def test_package_version() -> None:
