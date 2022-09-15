0d4d2c5f6bf1ba5cb88bcc373f45e42810bc4565
Melly Burns <9437821+TheMellyBee@users.noreply.github.com>
2022-09-15 14:58:27 -0700

    StandardResolver and Supporting classes (#779)
    * First almost direct port of resolver, dataschema and ColumnSchema
    
    * Add a factory for the StandardMetrics
    
    * Add data typing to match types to fractional, integer, and string
    
    * ColumnSchema and DatasetSchema with tests
    
    * Moves file
    
    * Code and testing for StandardMetric (note this is a factory were we will need to add the rest of the metrics that are currently commented out after ints are done)
    
    * Reolver, StandardResolver, and tests
    
    * Java Linter
    
    * Adding in privacy and an lombrok constructor instead
    
    * Changes based off review
    
    * Refactor to not use the StandardMetric.java partial factory. Allow resolver to directly call zero() as it builds instead of reflection or through another point
    
    * Updates data schema to show dirty if there is a change to the columns and if not then true for no columns added

---
+++ java/core/src/test/java/com/whylogs/core/TestDataTypes.java
@@ -0,0 +1,16 @@
+package com.whylogs.core;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class TestDataTypes {
+
+  @Test
+  public void test_enum_datatypes() {
+    DataTypes dataTypes = DataTypes.Integral;
+    Assert.assertEquals(dataTypes.name(), "Integral");
+    Assert.assertTrue(dataTypes.includes(Integer.class));
+    Assert.assertFalse(dataTypes.includes(String.class));
+  }
+}
