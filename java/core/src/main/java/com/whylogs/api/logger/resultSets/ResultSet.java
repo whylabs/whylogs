169e904bf73027e03d6643f6e84e7a7289e92b6f
TheMellyBee <info@melanieburns.net>
2022-09-22 11:55:47 -0700

    Holding place - figuring out why the view isn't visible

--- java/core/src/main/java/com/whylogs/api/logger/resultSets/ResultSet.java
+++ java/core/src/main/java/com/whylogs/api/logger/resultSets/ResultSet.java
@@ -1,4 +1,18 @@
 package com.whylogs.api.logger.resultSets;
 
-public class ResultSet {
+/**
+    A holder object for profiling results.
+
+    A whylogs.log call can result in more than one profile. This wrapper class
+    simplifies the navigation among these profiles.
+
+    Note that currently we only hold one profile but we're planning to add other
+    kinds of profiles such as segmented profiles here.
+**/
+public abstract class ResultSet {
+
+    public static ResultSet read(String multiProfileFile){
+
+    }
+
 }
