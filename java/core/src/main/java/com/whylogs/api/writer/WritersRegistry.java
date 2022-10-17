24aff919df99795bfd77dc8dee2e73d90186651a
TheMellyBee <info@melanieburns.net>
2022-10-17 15:57:22 -0700

    Getting skeletons for Writers and writable in. Will need to think through arguments passing in Java

---
+++ java/core/src/main/java/com/whylogs/api/writer/WritersRegistry.java
@@ -0,0 +1,8 @@
+package com.whylogs.api.writer;
+
+public class WritersRegistry {
+    public static  <T extends Writer> T get(String name) {
+        // TODO: Not implemented yet
+        return null;
+    }
+}
