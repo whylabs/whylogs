8e27718c0dbcd0d4bc6c8483cf9342ff05b957ea
Felipe Adachi <felipeadachi@gmail.com>
2022-11-17 09:50:26 -0300

    fix TestReports

--- python/tests/viz/extensions/test_reports.py
+++ python/tests/viz/extensions/test_reports.py
@@ -15,21 +15,21 @@ class TestReports(object):
         return ProfileSummaryReport(target_view=profile_view)
 
     def test_summary_drift_report_returns_html(self, summary_drift_report):
-        html_report = summary_drift_report.generate_constraints_report()
+        html_report = summary_drift_report.report()
         assert isinstance(html_report, HTML)
         assert "<div>" in html_report.data
 
     def test_exception_if_not_both_profiles(self, profile_view):
         with pytest.raises(ValueError, match="This method has to get both target and reference profiles"):
             report = SummaryDriftReport(ref_view=profile_view, target_view=None)
-            report.generate_constraints_report()
+            report.report()
 
     def test_summary_profile_returns_html(self, profile_summary_report):
-        html_report = profile_summary_report.generate_constraints_report()
+        html_report = profile_summary_report.report()
         assert isinstance(html_report, HTML)
         assert "<div>" in html_report.data
 
     def test_exception_if_not_profile(self, profile_view):
         with pytest.raises(ValueError, match="This method has to get target Dataset Profile View"):
             report = ProfileSummaryReport(target_view=None)
-            report.generate_constraints_report()
+            report.report()
