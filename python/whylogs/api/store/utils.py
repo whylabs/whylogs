4d14d29455c647e93a84e133bd688ce52f697f95
murilommen <murilommen@gmail.com>
2022-11-23 17:00:34 -0300

    removing config.ini and get_env methods

--- python/whylogs/api/store/utils.py
+++
@@ -1,8 +0,0 @@
-import configparser
-import os
-
-
-def get_env(environment, variable) -> str:
-    cfg = configparser.ConfigParser()
-    cfg.read("config.ini")
-    return cfg.get(environment, variable, vars=os.environ)
