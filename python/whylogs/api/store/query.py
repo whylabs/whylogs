303314afb97a3d5eaae5d683bab7e88a91823272
murilommen <murilommen@gmail.com>
2022-11-29 19:57:56 -0300

    making query a union of dataclasses

--- python/whylogs/api/store/query.py
+++ python/whylogs/api/store/query.py
@@ -1,36 +1,26 @@
-from abc import ABC
+from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Union
 
 
-class BaseQuery(ABC):
-    def __init__(self):
-        self.dataset_id = None
+@dataclass
+class DateQuery:
+    dataset_id: str
+    start_date: datetime
+    end_date: Optional[datetime] = field(default=None)
+    segment_tag: Optional[str] = field(default=None)
 
-
-class DateQuery(BaseQuery):
-    def __init__(
-        self,
-        dataset_id: str,
-        start_date: datetime,
-        end_date: Optional[datetime] = None,
-        segment_tag: Optional[str] = None,
-    ):
-
-        super().__init__()
-        self.dataset_id = dataset_id
-        self.start_date = start_date
-        self.end_date = end_date
-        self.segment_tag = segment_tag
-
+    def __post_init__(self):
         if self.end_date is None:
             self.end_date = self.start_date
         if self.end_date < self.start_date:
             self.start_date, self.end_date = self.end_date, self.start_date
 
 
-class DatasetIdQuery(BaseQuery):
-    def __init__(self, dataset_id: str, segment_tag: Optional[str] = None):
-        super().__init__()
-        self.dataset_id = dataset_id
-        self.segment_tag = segment_tag
+@dataclass
+class DatasetIdQuery:
+    dataset_id: str
+    segment_tag: Optional[str] = field(default=None)
+
+
+BaseQuery = Union[DateQuery, DatasetIdQuery]
