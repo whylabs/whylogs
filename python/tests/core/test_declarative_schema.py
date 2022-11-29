b69eee5ca49281247705eca9ea582821696459bd
Richard Rogers <richard@whylabs.ai>
2022-11-29 03:07:34 -0800

    fixes

--- python/tests/core/test_declarative_schema.py
+++ python/tests/core/test_declarative_schema.py
@@ -1,6 +1,10 @@
-from whylogs.core.declarative_schema import DeclarativeSchema, MetricSpc, ResolverSpec
+import pytest
+
+import whylogs as why
+from whylogs.core.datatypes import String
+from whylogs.core.declarative_schema import DeclarativeResolver, DeclarativeSchema, MetricSpec, ResolverSpec
 from whylogs.core.metrics import StandardMetric
-from whylogs.core.metrics.condition_count_metric import ConditionCountConfig, ConditionCountMetric
+from whylogs.core.metrics.condition_count_metric import Condition, ConditionCountConfig, ConditionCountMetric
 from whylogs.core.metrics.condition_count_metric import Relation as Rel
 from whylogs.core.metrics.condition_count_metric import relation as rel
 
@@ -12,7 +16,7 @@ def test_declarative_schema() -> None:
             ResolverSpec(
                 column_name = "column_1",
                 metrics = [
-                    MetricSpec(StandardMetric.distribution),
+                    MetricSpec(StandardMetric.distribution.value),
                     MetricSpec(
                         ConditionCountMetric,
                         ConditionCountConfig(
@@ -27,7 +31,7 @@ def test_declarative_schema() -> None:
             ResolverSpec(
                 column_type = String,
                 metrics = [
-                    MetricSpec(StandardMetric.frequent_items),
+                    MetricSpec(StandardMetric.frequent_items.value),
                     MetricSpec(
                         ConditionCountMetric,
                         ConditionCountConfig(
@@ -42,6 +46,24 @@ def test_declarative_schema() -> None:
         ]
     )
 
+    data = {"column_1": 3.14, "column_2": "lmno"}
+    results = why.log(row=data, schema = schema).view()
+    col1_metrics = set(results.get_column("column_1").get_metric_names())
+    assert col1_metrics == {"distribution", "condition_count"}
+    col1_conditions = set(results.get_column("column_1").get_metric("condition_count").conditions.keys())
+    assert col1_conditions == {"below 42", "above 42"}
 
-def test_invalid_config() -> None:
-    pass
+    col2_metrics = set(results.get_column("column_2").get_metric_names())
+    assert col2_metrics == {"frequent_items", "condition_count"}
+    col2_conditions = set(results.get_column("column_2").get_metric("condition_count").conditions.keys())
+    assert col2_conditions == {"alpha", "digit"}
+
+
+@pytest.mark.parametrize("spec,msg", [
+    (ResolverSpec(), "DeclarativeSchema: resolver specification must supply name or type"),
+    (ResolverSpec(column_type = str), "DeclarativeSchema: resolver specification column type must be a DataType"),
+    (ResolverSpec(column_name = "bruce", metrics = [MetricSpec(int)]), "DeclarativeSchema: must supply a Metric subclass to MetricSpec"),
+])
+def test_invalid_config(spec, msg) -> None:
+    with pytest.raises(ValueError, match=msg):
+        DeclarativeResolver([spec])
--- python/whylogs/core/declarative_schema.py
+++ python/whylogs/core/declarative_schema.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Mapping, Optional, TypeVar
+import logging
 
 from whylogs.core.datatypes import DataType, TypeMapper
 from whylogs.core.metrics.metrics import Metric, MetricConfig
 from whylogs.core.resolvers import Resolver
-from whylogs.core.schema import DatasetSchema
+from whylogs.core.schema import DatasetSchema, ColumnSchema
 from whylogs.core.segmentation_partition import SegmentationPartition
 
 logger = logging.getLogger(__name__)
@@ -19,7 +20,7 @@ class MetricSpec:
 @dataclass
 class ResolverSpec:
     column_name: Optional[str] = None  # TODO: maybe make this a regex
-    column_type: Optional[DataType] = None
+    column_type: Optional[Any] = None  # TODO: must be <: DataType
     metrics: List[MetricSpec] = field(default_factory=list)
 
 
@@ -28,7 +29,7 @@ class DeclarativeResolver(Resolver):
     def __init__(
             self,
             resolvers: List[ResolverSpec],
-            default_config: Optional[MetricConfig]
+            default_config: Optional[MetricConfig] = None
     ) -> None:
         # Validate resolvers -- must have name xor type, MetricSpec metrcis must <: Metric
         for spec in resolvers:
@@ -36,9 +37,14 @@ class DeclarativeResolver(Resolver):
                 logger.warning(f"DeclarativeSchema: column {spec.column_name} also specified type, name takes precedence")
             if not (spec.column_name or spec.column_type):
                 raise ValueError("DeclarativeSchema: resolver specification must supply name or type")
+
+            if spec.column_type and not issubclass(spec.column_type, DataType):
+                raise ValueError("DeclarativeSchema: resolver specification column type must be a DataType")
+
             for metric_spec in spec.metrics:
                 if not issubclass(metric_spec.metric, Metric):
                     raise ValueError("DeclarativeSchema: must supply a Metric subclass to MetricSpec")
+            # TODO: should we allow metric list to be empty?
 
         self._resolvers = resolvers
 
@@ -51,11 +57,11 @@ class DeclarativeResolver(Resolver):
         result: Dict[str, Metric] = {}
         for resolver_spec in self._resolvers:
             col_name, col_type = resolver_spec.column_name, resolver_spec.column_type
-            if (col_name and col_name == name) or isinstance(why_type, col_type):
+            if (col_name and col_name == name) or (col_name is None and isinstance(why_type, col_type)):
                 for spec in resolver_spec.metrics:
-                    result[spec.metric.get_namespace()] = metric.zero(spec.config or colunn_schema.cfg)
-
-        return reuslt
+                    result[spec.metric.get_namespace()] = spec.metric.zero(spec.config or column_schema.cfg)
+        # TODO: complain if result is empty
+        return result
 
 
 class DeclarativeSchema(DatasetSchema):
@@ -73,12 +79,12 @@ class DeclarativeSchema(DatasetSchema):
         if not resolvers:
             logger.error("No columns specified in DeclarativeSchema")
         resolver = DeclarativeResolver(resolvers, default_config)
-        super(self, DatasetSchema).__init__(
-            types,
-            default_config,
-            type_mapper,
-            resolver,
-            cache_size,
-            schema_based_automerge,
-            segments
+        super().__init__(
+            types = types,
+            default_configs = default_config,
+            type_mapper = type_mapper,
+            resolvers = resolver,
+            cache_size = cache_size,
+            schema_based_automerge = schema_based_automerge,
+            segments = segments
         )
