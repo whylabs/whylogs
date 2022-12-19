a8c2bb7991e97b14c2e5009fd15709ce054bc184
Jamie Broomall <jamie@whylabs.ai>
2022-12-19 13:05:13 -0800

    release 1.1.18-dev0

--- python/tests/smoketest.py
+++ python/tests/smoketest.py
@@ -12,7 +12,7 @@ intended to test the wheel in a production environment,
 not a development environment.
 """
 
-current_version = "1.1.17"
+current_version = "1.1.18-dev0"
 
 
 def test_package_version() -> None:
