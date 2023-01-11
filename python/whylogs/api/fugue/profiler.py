323c6dfbf78bb1c6f3ddcd5dc146a19718065f01
Han Wang <hwang@lyft.com>
2023-01-11 09:10:30 -0800

    Upgrade Fugue (#1048)
    This PR upgrades Fugue to 0.8.0, it supports arbitrary column names, so
    we remove all the workarounds and the unit tests were not changed, and
    they pass.
    
    * Upgrade to Fugue 0.8.0
    * Remove workarounds
    * Update notebooks

--- python/whylogs/api/fugue/profiler.py
+++ python/whylogs/api/fugue/profiler.py
@@ -2,19 +2,9 @@ from datetime import datetime, timezone
 from functools import reduce
 from typing import Any, Dict, Iterable, List, Optional
 
+import fugue.api as fa
 import pandas as pd
-from fugue import (
-    FugueWorkflow,
-    PartitionSpec,
-    Schema,
-    make_execution_engine,
-    transform,
-    transformer,
-)
-from fugue.dataframe.utils import (
-    normalize_dataframe_column_names,
-    rename_dataframe_column_names,
-)
+from fugue import PartitionSpec, Schema, transformer
 
 import whylogs as why
 from whylogs.core import DatasetSchema
@@ -39,14 +29,9 @@ def fugue_profile(
     engine_conf: Any = None,
     **kwargs,
 ) -> Any:
-    if not isinstance(df, str):
-        df, renames = normalize_dataframe_column_names(df)
-    else:
-        renames = {}
     profiler = _FugueProfiler(
         partition,
         profile_cols,
-        renames=renames,
         dataset_timestamp=dataset_timestamp,
         creation_timestamp=creation_timestamp,
         schema=schema,
@@ -65,7 +50,6 @@ class _FugueProfiler:
         self,
         partition,
         cols,
-        renames: Dict[str, Any],
         dataset_timestamp: Optional[datetime] = None,
         creation_timestamp: Optional[datetime] = None,
         schema: Optional[DatasetSchema] = None,
@@ -75,11 +59,9 @@ class _FugueProfiler:
 
         self._dataset_timestamp = dataset_timestamp or now
         self._creation_timestamp = creation_timestamp or now
-        self._cols_to_orig = renames
-        self._orig_to_cols = {v: k for k, v in renames.items()}
 
         part = PartitionSpec(partition)
-        self._by = [self._orig_to_cols.get(x, x) for x in part.partition_by]
+        self._by = part.partition_by
         self._partition = PartitionSpec(part, by=self._by)
         self._cols = cols
         self._profile_field = profile_field
@@ -87,8 +69,6 @@ class _FugueProfiler:
         self._schema = schema
 
     def to_col_profiles(self, df: pd.DataFrame) -> Iterable[Dict[str, Any]]:
-        if len(self._cols_to_orig) > 0:
-            df = df.rename(columns=self._cols_to_orig)
         res = why.log(df[self._cols] if self._cols is not None else df, schema=self._schema)
         for col_name, col_profile in res.view().get_columns().items():
             yield {_COL_NAME_FIELD: col_name, _COL_PROFILE_FIELD: col_profile.serialize()}
@@ -99,41 +79,37 @@ class _FugueProfiler:
         )
         return df.head(1).assign(**{_COL_PROFILE_FIELD: merged_profile.serialize()})
 
-    def merge_to_view(self, col_profiles: List[Dict[str, Any]]) -> Iterable[Dict[str, Any]]:
-        profile_view = DatasetProfileView(
+    def merge_to_view(self, col_profiles: Iterable[Dict[str, Any]]) -> DatasetProfileView:
+        return DatasetProfileView(
             columns={
                 row[_COL_NAME_FIELD]: ColumnProfileView.from_bytes(row[_COL_PROFILE_FIELD]) for row in col_profiles
             },
             dataset_timestamp=self._dataset_timestamp,
             creation_timestamp=self._creation_timestamp,
         )
-        yield {self._profile_field: profile_view.serialize()}
 
     def profile_partition(self, df: pd.DataFrame) -> pd.DataFrame:
-        if len(self._cols_to_orig) > 0:
-            pdf = df.rename(columns=self._cols_to_orig)
-        else:
-            pdf = df
-        res = why.log(pdf[self._cols] if self._cols is not None else pdf, schema=self._schema).view().serialize()
+        res = why.log(df[self._cols] if self._cols is not None else df, schema=self._schema).view().serialize()
         return df.head(1)[self._by].assign(**{self._profile_field: res})  # type: ignore
 
     # ---------------- Starting Fugue related logic
 
     def transform_no_logical_partition(self, df: Any, engine: Any, engine_conf: Any) -> DatasetProfileView:
-        dag = FugueWorkflow()
-        input_df = dag.load(df) if isinstance(df, str) else dag.df(df)
-        cols = input_df.partition(self._partition).transform(
-            self.to_col_profiles,
-            schema=[(_COL_NAME_FIELD, str), (_COL_PROFILE_FIELD, bytes)],
-        )
-        merged_cols = cols.partition_by(_COL_NAME_FIELD).transform(self.merge_col_profiles, schema="*")
-        result = merged_cols.process(self.merge_to_view, schema=self._profile_schema)
-        result.yield_dataframe_as("result", as_local=True)
-        engine = make_execution_engine(engine, engine_conf)
-        return DatasetProfileView.deserialize(dag.run(engine)["result"].as_array()[0][0])
+        with fa.engine_context(engine, engine_conf):
+            input_df = fa.load(df, as_fugue=True) if isinstance(df, str) else fa.as_fugue_df(df)
+            cols = fa.transform(
+                input_df,
+                self.to_col_profiles,
+                schema=[(_COL_NAME_FIELD, str), (_COL_PROFILE_FIELD, bytes)],
+                partition=self._partition,
+            )
+            merged_cols = fa.transform(
+                cols, self.merge_col_profiles, schema="*", partition=_COL_NAME_FIELD, as_local=True
+            )
+            return self.merge_to_view(fa.as_dict_iterable(merged_cols))
 
     def transform_with_logical_partition(self, df: Any, engine: Any, engine_conf: Any, **kwargs: Any) -> Any:
-        res = transform(
+        return fa.transform(
             df,
             transformer(lambda pdf: pdf.schema.extract(self._by) + self._profile_schema)(self.profile_partition),
             partition=self._partition,
@@ -141,4 +117,3 @@ class _FugueProfiler:
             engine_conf=engine_conf,
             **kwargs,
         )
-        return rename_dataframe_column_names(res, self._cols_to_orig)
