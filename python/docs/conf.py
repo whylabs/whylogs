2811c4a7ba54e0f200bdb8c15e63b79f8461e57f
Anthony Naddeo <anthony@whylabs.ai>
2023-03-04 13:38:59 -0800

    Create release for 1.1.29-dev2

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -8,7 +8,7 @@ if shutil.which("pandoc") is None:
     print("Pandoc is required to build our documentation.")
     sys.exit(1)
 
-version = "1.1.28"
+version = "1.1.29-dev2"
 
 project = "whylogs"
 author = "whylogs developers"
