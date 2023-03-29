6c71448b99467797248dd0d89e4421d4df32b744
naddeoa <anthony.naddeo@gmail.com>
2023-03-29 08:20:28 -0700

    Fix monthly grouping in the multi logger, add year support (#1166)
    Apparently the first of the month is 1, not 0.
    
    Also adding year support for demo purposes.
    
    I added the exception case to make sure that something goes wrong if the
    user isn't using types and supplies something that isn't a valid time granularity.
    
    Co-authored-by: Anthony Naddeo <anthony@whylabs.ai>

--- python/tests/api/logger/experimental/multi_dataset_logger/test_time_util.py
+++ python/tests/api/logger/experimental/multi_dataset_logger/test_time_util.py
@@ -4,7 +4,32 @@ from whylogs.api.logger.experimental.multi_dataset_logger.time_util import (
     FunctionTimer,
     Schedule,
     TimeGranularity,
+    truncate_time_ms,
 )
+
+
+def test_truncate_day() -> None:
+    ts = 1680038612000  # Tuesday, March 28, 2023 9:23:32 PM
+    expected = 1679961600000  # Tuesday, March 28, 2023 0:00:00
+    assert truncate_time_ms(ts, TimeGranularity.Day) == expected
+
+
+def test_truncate_hour() -> None:
+    ts = 1680038612000  # Tuesday, March 28, 2023 9:23:32 PM
+    expected = 1680037200000  # Tuesday, March 28, 2023 21:00:00
+    assert truncate_time_ms(ts, TimeGranularity.Hour) == expected
+
+
+def test_truncate_month() -> None:
+    ts = 1680038612000  # Tuesday, March 28, 2023 9:23:32 PM
+    expected = 1677628800000  # Wednesday, March 1, 2023 0:00:00
+    assert truncate_time_ms(ts, TimeGranularity.Month) == expected
+
+
+def test_truncate_year() -> None:
+    ts = 1680038612000  # Tuesday, March 28, 2023 9:23:32 PM
+    expected = 1672531200000  # Sunday, January 1, 2023 0:00:00
+    assert truncate_time_ms(ts, TimeGranularity.Year) == expected
 
 
 def test_seconds_work() -> None:
