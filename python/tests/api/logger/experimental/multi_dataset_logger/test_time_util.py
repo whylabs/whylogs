90ce449e0e684ff0d0b685768b21fce069b00f33
richard-rogers <93153899+richard-rogers@users.noreply.github.com>
2023-03-30 22:39:15 -0700

    Update python/tests/api/logger/experimental/multi_dataset_logger/test_time_util.py

--- python/tests/api/logger/experimental/multi_dataset_logger/test_time_util.py
+++ python/tests/api/logger/experimental/multi_dataset_logger/test_time_util.py
@@ -7,7 +7,7 @@ from whylogs.api.logger.experimental.multi_dataset_logger.time_util import (
     TimeGranularity,
 )
 
-# A sequence of times get instantiated, so we need global state for them
+# A sequence of timers get instantiated, so we need global state for them
 # to simulate universal time.
 
 _FAKE_TIMES: List[float] = []  # list of times (seconds) where the FakeTimer
