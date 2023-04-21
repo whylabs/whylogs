7beed41aad218f772d344fcf23d23700f747f414
murilommen <murilommen@gmail.com>
2023-04-21 16:50:33 -0300

    included log_requests as standard

--- python/tests/api/whylabs/test_log_reference.py
+++ python/tests/api/whylabs/test_log_reference.py
@@ -3,18 +3,17 @@ import os
 import pandas as pd
 
 import whylogs as why
-from whylogs.api.whylabs.log_reference import log_reference
+from whylogs.api.whylabs.log_reference import log_references
 
 
 def test_log_reference() -> None:
     os.environ["WHYLABS_HOST"] = "https://songbird.development.whylabsdev.com"
     df = pd.DataFrame({"a": [1, 2, 3, 4]})
+    df2 = df.copy()
 
     why.init(anonymous=True)
 
-    result = log_reference(df)
+    req_results = log_references([{"data": df}, {"data": df2}])
 
-    assert result.status_code == 200
-
-
-# TODO guarantee it is possible to change alias + timestamp
+    for result in req_results:
+        assert result.status_code == 200
--- python/whylogs/api/whylabs/log_reference.py
+++ python/whylogs/api/whylabs/log_reference.py
@@ -1,12 +1,14 @@
 import logging
 import os
+from dataclasses import dataclass
 from datetime import datetime
 from tempfile import NamedTemporaryFile
-from typing import Any, Optional
+from typing import Any, List, Optional
 
 import requests
 from whylabs_client import ApiClient, Configuration
 from whylabs_client.api.sessions_api import SessionsApi
+from whylabs_client.model.batch_log_reference_request import BatchLogReferenceRequest
 from whylabs_client.model.log_reference_request import LogReferenceRequest
 
 import whylogs as why
@@ -18,6 +20,13 @@ from whylogs.api.whylabs.session_manager import (
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class DataInput:
+    data: Any
+    alias: Optional[str] = None
+    dataset_timestamp: Optional[datetime] = None
 
 
 def check_if_session_is_active() -> bool:
@@ -44,43 +53,28 @@ def get_sessions_api(client: ApiClient) -> SessionsApi:
     return SessionsApi(client)
 
 
-def create_upload_reference_url(
+def create_upload_references_url(
     api: SessionsApi,
     session_manager: SessionManager,
-    alias: Optional[str] = None,
-    dataset_timestamp: Optional[datetime] = None,
+    references: List[DataInput],
 ) -> Any:
     if not isinstance(session_manager.session, GuestSession):
         raise Exception("It's not possible to log profiles to guest session on a logged session")
 
-    dataset_timestamp = dataset_timestamp or datetime.now()
-    dataset_timestamp_epoch = int(1000 * dataset_timestamp.timestamp())
+    references_request_list = []
+    for reference in references:
+        references_request_list.append(
+            LogReferenceRequest(alias=reference.alias, dataset_timestamp=reference.dataset_timestamp)
+        )
 
-    response = api.create_reference_profile_upload(
+    response = api.batch_create_reference_profile_upload(
         session_id=session_manager.session.session_id,
-        log_reference_request=LogReferenceRequest(alias=alias, dataset_timestamp=dataset_timestamp_epoch),
+        batch_log_reference_request=BatchLogReferenceRequest(references=references_request_list),
     )
     return response
 
 
-def log_reference(data: Any, alias: Optional[str] = None, dataset_timestamp: Optional[datetime] = None):
-    assert check_if_session_is_active(), "You need an active session. Call why.init() to do so."
-    assert check_if_session_is_guest(), "This method can only be used with an initialized GuestSession"
-
-    sm = SessionManager.get_instance()
-
-    # 1. Get the API client with get_client() and assign it to `api_client`
-    api_client = get_client(session_manager=sm)
-
-    # 2. Get API from client with get_sessions_api(api_client) and assign it to `api`
-    api = get_sessions_api(client=api_client)
-    # 3. Create upload URL with url=create_upload_reference_url(api, alias: Optional[str] = None, dataset_timestamp: Optional[datetime]=None)
-    url_info = create_upload_reference_url(
-        api=api, session_manager=sm, alias=alias, dataset_timestamp=dataset_timestamp
-    )
-    # 4. Log incoming data with why.log(data) and assign to variable `results`
-    results = why.log(data)
-    # 5. Write `results` to a tempfile.NamedTemporaryFile() and assign it to `tmp_file`
+def write_results(results: why.ResultSet, url_info: Any):
     with NamedTemporaryFile() as tmp_file:
         results.view().write(file=tmp_file)
         tmp_file.flush()
@@ -98,4 +92,53 @@ def log_reference(data: Any, alias: Optional[str] = None, dataset_timestamp: Opt
         return response
 
 
+def validate_and_get_session_manager() -> SessionManager:
+    assert check_if_session_is_active(), "You need an active session. Call why.init() to do so."
+    assert check_if_session_is_guest(), "This method can only be used with an initialized GuestSession"
+
+    sm = SessionManager.get_instance()
+    return sm
+
+
 # TODO make sure return response is actually useful
+
+
+def validate_references_list(references: List[DataInput]) -> None:
+    if isinstance(references, dict):
+        references = [references]
+
+    if not isinstance(references, List):
+        raise ValueError("You must define a list of DataInput objects to use this method")
+
+    for i, reference in enumerate(references):
+        if not isinstance(reference, DataInput):
+            references[i] = DataInput(reference)
+
+    if len(references) > 3:
+        raise ValueError("You can only set 3 reference profiles at once")
+
+
+def turn_value_into_timestamp(datetime_value: Optional[datetime] = None) -> int:
+    datetime_value = datetime_value or datetime.now()
+    return int(1000 * datetime_value.timestamp())
+
+
+def log_references(references: List[DataInput]):
+    validate_references_list(references=references)
+
+    for data_input in references:
+        data_input.dataset_timestamp = turn_value_into_timestamp(datetime_value=data_input.dataset_timestamp)  # type: ignore
+
+    sm = validate_and_get_session_manager()
+    api_client = get_client(session_manager=sm)
+    api = get_sessions_api(client=api_client)
+
+    url_info = create_upload_references_url(api=api, session_manager=sm, references=references)
+
+    responses = []
+
+    for i, data_input in enumerate(references):
+        result_set = why.log(data_input.data)
+        responses.append(write_results(results=result_set, url_info=url_info.references[i]))
+
+    return responses
