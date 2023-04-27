2a4422259e2ee85000a59e4c172a9d429f6e173d
Richard Rogers <richard@whylabs.ai>
2023-04-26 17:47:13 -0700

    work

--- python/whylogs/experimental/extras/confighub.py
+++ python/whylogs/experimental/extras/confighub.py
@@ -1,6 +1,8 @@
 
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Tuple
+
+import os
 
 from semantic_version import Version
 
@@ -20,21 +22,38 @@ class ConfigStore(ABC):
         pass
 
     @abstractmethod
-    def get_version(self, ver: Version) -> str:
-        """Returns requested version of the config. ValueError on invalid/unavailable"""
+    def get_version(self, ver: Version) -> Tuple[str, str]:
+        """
+        Returns the requested version of the config. ValueError on invalid/unavailable.
+        """
         pass
 
     @abstractmethod
-    def get_available_versions(self) -> List[Version]:  # List[Tuple[Version, timestamp]] ?
-        """Returns available versions in ascending order, [1.0.0] if it's the first version"""
+    def get_available_versions(self) -> List[Tuple[Version, str]]:
+        """
+        Lists available versions in ascending order, [1.0.0] if it's the first version
+        returns (Version, note), where note = commit message attached to the version
+        """
         pass
 
     @abstractmethod
-    def propose_version(self, config: str, new_version: Version) -> None:
-        """Submit config to review process with proposed new verison. ValueError if new_version <= latest"""
+    def propose_version(self, config: str, new_version: Version, note: str = "") -> None:
+        """
+        Submit config to review process with proposed new verison. ValueError if new_version <= latest"""
         pass
 
     @abstractmethod
-    def commit_verison(self, config: str, new_version: Version) -> None:
+    def commit_verison(self, config: str, new_version: Version, note: str = "") -> None:
         """Make config the latest version. ValueError if new_version < previous latest version"""
         pass
+
+
+class LocalGitConfigStore(ConfigStore):
+    _repo_path = "/tmp"
+
+    def __init__(self, org_id: str, dataset_id: str, what: str):
+        super().__init__(org_id, dataset_id, what)
+        path = f"{self._repo_path}/{org_id}/{dataset_id}"
+        if not os.path.isdir(path):
+            os.makedirs(path)
+            os.system(
