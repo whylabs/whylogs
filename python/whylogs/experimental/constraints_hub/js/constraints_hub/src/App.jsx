1963b4cedc44fc26f288a7ab59d44ae0d054e649
murilommen <murilommen@gmail.com>
2023-04-28 13:41:20 -0400

    integrated with backend

--- python/whylogs/experimental/constraints_hub/js/constraints_hub/src/App.jsx
+++ python/whylogs/experimental/constraints_hub/js/constraints_hub/src/App.jsx
@@ -4,20 +4,24 @@ import {
   Flex,
   Button,
   useDisclosure,
+  Heading,
   Table,
   Thead,
   Tr,
   Th,
   Tbody,
   Td,
+  Select,
 } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import ModalComp from "./components/ModalComp"
+import axios from "axios"
 
 const App = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [data, setData] = useState([])
   const [dataEdit, setDataEdit] = useState({})
+  const [version, setVersion] = useState("1.0.0")
 
   useEffect(() => {
     const db_costumer = localStorage.getItem("constraint_set")
@@ -40,11 +44,33 @@ const App = () => {
     localStorage.setItem("constraint_set", JSON.stringify(newArray))
   }
 
+  const convertConstraints = (input) => {
+    const constraints = input.map((constraint) => {
+      const { column_name: column_name, data_type: metric } = constraint.column;
+      const { constraint: factory, cons_value } = constraint;
+      const name = `${column_name}_${factory}_${metric}`;
+      return { column_name, factory, metric, name, ...cons_value };
+    });
+  
+    return { constraints_json: { constraints } };
+  };
+
   const handlePush = () => {
-    console.log("// TODO")
+    const localConstraints = JSON.parse(localStorage.getItem("constraint_set"))
+    const body = convertConstraints(localConstraints)
+    console.log(body)
+    axios.post('http://localhost:8000/push_constraints', body)
+      .then(response => {
+        console.log('Successfully sent constraint:', response.data)
+      })
+      .catch(error => {
+        console.error('Failed to send constraint:', error)
+      })
+      setVersion(response.data)
   }
 
   return (
+    
     <Flex
       h="100vh"
       align="center"
@@ -52,14 +78,22 @@ const App = () => {
       fontSize="20px"
       fontFamily="poppins"
     >
+      
       <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
         <Box overflowY="auto" height="90%">
+          <Heading>WhyLabs Constraints Hub</Heading>
+          <Select
+              placeholder={version}
+              value={version}
+              py={5}>
+              
+          </Select>
           <Table mt="6">
             <Thead>
               <Tr>
-                <Th fontSize="20px">Column</Th>
-                <Th fontSize="20px">Constraint</Th>
-                <Th fontSize="20px">Values</Th>
+                <Th fontSize="15px">Column</Th>
+                <Th fontSize="15px">Constraint</Th>
+                <Th fontSize="15px">Values</Th>
 
                 <Th p={0}></Th>
                 <Th p={0}></Th>
@@ -68,7 +102,7 @@ const App = () => {
             <Tbody>
               {data.map(({ column, constraint, cons_value }, index) => (
                 <Tr key={index} cursor="pointer " _hover={{ bg: "gray.100" }}>
-                  <Td>{column.col_name}</Td>
+                  <Td>{column.column_name}</Td>
                   <Td>{constraint}</Td>
                   <Td>{JSON.stringify(cons_value)}</Td>
                   <Td p={0}>
--- python/whylogs/experimental/constraints_hub/js/constraints_hub/src/components/ModalComp.jsx
+++ python/whylogs/experimental/constraints_hub/js/constraints_hub/src/components/ModalComp.jsx
@@ -17,17 +17,10 @@ import { useState, useEffect } from "react"
 import { useFetchData } from "../hooks/useFetchData"
 
 const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
-  // TODO Fetch this from entity schema API
-  const columnOptions = [
-    {
-      col_name: "col1",
-      col_type: "integral",
-    },
-    {
-      col_name: "col2",
-      col_type: "fractional",
-    },
-  ]
+  const [columnEntitySchema] = useFetchData(
+    "http://localhost:8000/entity_schema"
+  )
+  const columnOptions = columnEntitySchema?.entity_schema ?? []
 
   const [columnTypesDict] = useFetchData(
     "http://localhost:8000/types_to_constraints"
@@ -36,33 +29,37 @@ const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
     Object.entries(columnTypesDict?.constraints_per_datatype ?? {})
   )
 
-  // TODO Fetch this from API
-  const constraintsToValues = new Map([
-    ["no_mising_values", []],
-    ["is_in_range", ["lower", "upper"]],
-    ["column_is_probably_unique", []],
-    ["distinct_number_in_range", ["upper", "lower"]],
-  ])
-
   const [column, setColumn] = useState(dataEdit.column || undefined)
   const [constraint, setConstraint] = useState(dataEdit.constraint || "")
   const [cons_value, setConsValue] = useState(dataEdit.cons_value || {})
 
+
   const renderConstraints = () => {
     if (!column || !columnTypesToConstraints) return null
-    const constraints = columnTypesToConstraints.get(column.col_type)
-    return constraints.map((option) => <option value={option}>{option}</option>)
+    const constraints = columnTypesToConstraints.get(column.data_type)
+    return constraints?.map((option) => <option value={option}>{option}</option>)
   }
 
   const handleConsValue = (target) => {
     setConsValue({ ...cons_value, [target.name]: target.value })
   }
 
+  const [constraintParameters] = useFetchData(
+    "http://localhost:8000/get_params",
+    {"my_string": constraint}
+  )
+
+  const constraintsToValues = new Map([
+    [constraint, [constraintParameters?.parameters]]
+  ])
+
+
 
   const renderConstraintValues = () => {
     if (!constraint) return null
-    
+    console.log(constraintsToValues)
     const constraint_value = constraintsToValues.get(constraint)
+    console.log(constraint_value)
 
     return constraint_value?.map((input) => (
       <FormLabel>
@@ -108,17 +105,17 @@ const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
                 <FormLabel>Column</FormLabel>
                 <Select
                   placeholder="Select a column"
-                  value={column?.col_name}
+                  value={column?.column_name}
                   onChange={(e) =>
                     setColumn(
                       columnOptions.find(
-                        (option) => option.col_name == e.target.value
+                        (option) => option.column_name == e.target.value
                       )
                     )
                   }
                 >
-                  {columnOptions.map((option) => (
-                    <option value={option.col_name}>{option.col_name}</option>
+                  {columnOptions?.map((option) => (
+                    <option value={option.column_name}>{option.column_name}</option>
                   ))}
                 </Select>
               </Box>
