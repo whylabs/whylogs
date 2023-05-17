cff6a78d5c6b16e8f15f18e3d0eb811b195e198b
Richard Rogers <richard@whylabs.ai>
2023-05-17 15:08:03 -0700

    dataset UDFs are all multicolumn; no type matching; better-ish exception handling

--- python/tests/experimental/core/test_udf_schema.py
+++ python/tests/experimental/core/test_udf_schema.py
@@ -15,73 +15,64 @@ from whylogs.experimental.core.udf_schema import (
 def test_udf_row() -> None:
     schema = UdfSchema(
         STANDARD_RESOLVER,
-        udf_specs=[UdfSpec(column_name="col1", udfs={"col2": lambda x: x, "col3": lambda x: x})],
+        udf_specs=[UdfSpec(column_names=["col1"], udfs={"col2": lambda x: x["col1"], "col3": lambda x: x["col1"]})],
     )
     data = {"col1": 42}
     results = why.log(row=data, schema=schema).view()
     col1 = results.get_column("col1").to_summary_dict()
-    col2 = results.get_column("col1.col2").to_summary_dict()
-    col3 = results.get_column("col1.col3").to_summary_dict()
+    col2 = results.get_column("col2").to_summary_dict()
+    col3 = results.get_column("col3").to_summary_dict()
     assert col1 == col2 == col3
 
 
 def test_udf_pandas() -> None:
     schema = UdfSchema(
         STANDARD_RESOLVER,
-        udf_specs=[UdfSpec(column_name="col1", udfs={"col2": lambda x: x, "col3": lambda x: x})],
+        udf_specs=[UdfSpec(column_names=["col1"], udfs={"col2": lambda x: x["col1"], "col3": lambda x: x["col1"]})],
     )
     data = pd.DataFrame({"col1": [42, 12, 7]})
     results = why.log(pandas=data, schema=schema).view()
     col1 = results.get_column("col1").to_summary_dict()
-    col2 = results.get_column("col1.col2").to_summary_dict()
-    col3 = results.get_column("col1.col3").to_summary_dict()
+    col2 = results.get_column("col2").to_summary_dict()
+    col3 = results.get_column("col3").to_summary_dict()
     assert col1 == col2 == col3
 
 
-@register_dataset_udf("col1")
+@register_dataset_udf(["col1"])
 def add5(x):
-    return x + 5
-
-
-@register_dataset_udf(col_type=String, udf_name="upper case")
-def upper(x):
-    return x.upper()
+    return x["col1"] + 5
 
 
 def square(x):
-    return x * x
+    return x["col1"] * x["col1"]
 
 
 def test_decorator_pandas() -> None:
-    extra_spec = UdfSpec("col1", None, {"sqr": square})
+    extra_spec = UdfSpec(["col1"], {"sqr": square})
     schema = generate_udf_dataset_schema([extra_spec], STANDARD_RESOLVER)
     data = pd.DataFrame({"col1": [42, 12, 7], "col2": ["a", "b", "c"]})
     results = why.log(pandas=data, schema=schema).view()
     col1_summary = results.get_column("col1").to_summary_dict()
     assert "distribution/n" in col1_summary
-    add5_summary = results.get_column("col1.add5").to_summary_dict()
+    add5_summary = results.get_column("add5").to_summary_dict()
     assert "distribution/n" in add5_summary
-    upper_summary = results.get_column("col2.upper case").to_summary_dict()
-    assert "cardinality/est" in upper_summary
-    sqr_summary = results.get_column("col1.sqr").to_summary_dict()
+    sqr_summary = results.get_column("sqr").to_summary_dict()
     assert "distribution/n" in sqr_summary
 
 
 def test_decorator_row() -> None:
-    extra_spec = UdfSpec("col1", None, {"sqr": square})
+    extra_spec = UdfSpec(["col1"], {"sqr": square})
     schema = generate_udf_dataset_schema([extra_spec], STANDARD_RESOLVER)
     results = why.log(row={"col1": 42, "col2": "a"}, schema=schema).view()
     col1_summary = results.get_column("col1").to_summary_dict()
     assert "distribution/n" in col1_summary
-    add5_summary = results.get_column("col1.add5").to_summary_dict()
+    add5_summary = results.get_column("add5").to_summary_dict()
     assert "distribution/n" in add5_summary
-    upper_summary = results.get_column("col2.upper case").to_summary_dict()
-    assert "cardinality/est" in upper_summary
-    sqr_summary = results.get_column("col1.sqr").to_summary_dict()
+    sqr_summary = results.get_column("sqr").to_summary_dict()
     assert "distribution/n" in sqr_summary
 
 
-@register_dataset_udf(["col1", "col2"], None, "product")
+@register_dataset_udf(["col1", "col2"], "product")
 def times(x):
     return x["col1"] * x["col2"]
 
@@ -107,7 +98,7 @@ def test_multicolumn_udf_pandas() -> None:
         ),
     ]
 
-    extra_spec = UdfSpec("col1", None, {"sqr": square})
+    extra_spec = UdfSpec(["col1"], {"sqr": square})
     schema = generate_udf_dataset_schema([extra_spec], count_only)
     data = pd.DataFrame({"col1": [42, 12, 7], "col2": [2, 3, 4], "col3": [2, 3, 4]})
     results = why.log(pandas=data, schema=schema).view()
@@ -117,11 +108,11 @@ def test_multicolumn_udf_pandas() -> None:
     assert "counts/n" in col2_summary
     col3_summary = results.get_column("col3").to_summary_dict()
     assert "counts/n" in col3_summary
-    add5_summary = results.get_column("col1.add5").to_summary_dict()
+    add5_summary = results.get_column("add5").to_summary_dict()
     assert "counts/n" in add5_summary
     prod_summary = results.get_column("product").to_summary_dict()
     assert prod_summary["counts/n"] == 3
-    sqr_summary = results.get_column("col1.sqr").to_summary_dict()
+    sqr_summary = results.get_column("sqr").to_summary_dict()
     assert "counts/n" in sqr_summary
     div_summary = results.get_column("ratio").to_summary_dict()
     assert div_summary["distribution/n"] == 3
@@ -144,7 +135,7 @@ def test_multicolumn_udf_row() -> None:
         ),
     ]
 
-    extra_spec = UdfSpec("col1", None, {"sqr": square})
+    extra_spec = UdfSpec(["col1"], {"sqr": square})
     schema = generate_udf_dataset_schema([extra_spec], count_only)
     data = {"col1": 42, "col2": 2, "col3": 2}
     results = why.log(row=data, schema=schema).view()
@@ -154,12 +145,35 @@ def test_multicolumn_udf_row() -> None:
     assert "counts/n" in col2_summary
     col3_summary = results.get_column("col3").to_summary_dict()
     assert "counts/n" in col3_summary
-    add5_summary = results.get_column("col1.add5").to_summary_dict()
+    add5_summary = results.get_column("add5").to_summary_dict()
     assert "counts/n" in add5_summary
     prod_summary = results.get_column("product").to_summary_dict()
     assert prod_summary["counts/n"] == 1
-    sqr_summary = results.get_column("col1.sqr").to_summary_dict()
+    sqr_summary = results.get_column("sqr").to_summary_dict()
     assert "counts/n" in sqr_summary
     div_summary = results.get_column("ratio").to_summary_dict()
     assert div_summary["distribution/n"] == 1
     assert len(results.get_column("ratio").get_metrics()) == 2  # Integral -> counts plus registered distribution
+
+
+n: int = 0
+
+
+@register_dataset_udf(["oops"])
+def exothermic(x):
+    global n
+    n += 1
+    if n < 3:
+        raise ValueError("kaboom")
+
+    return x["oops"]
+
+
+def test_udf_throws() -> None:
+    schema = generate_udf_dataset_schema()
+    df = pd.DataFrame({"oops": [1, 2, 3, 4], "ok": [5, 6, 7, 8]})
+    results = why.log(pandas=df, schema=schema).view()
+    assert "exothermic" not in results.get_columns()
+    # oops_summary = results.get_column("exothermic").to_summary_dict()
+    ok_summary = results.get_column("ok").to_summary_dict()
+    assert ok_summary["counts/n"] == 4
--- python/whylogs/experimental/core/udf_schema.py
+++ python/whylogs/experimental/core/udf_schema.py
@@ -1,11 +1,9 @@
 import logging
-import sys
-from collections import defaultdict
 from copy import deepcopy
 from dataclasses import dataclass, field
-from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple, Union
+from typing import Any, Callable, Dict, List, Mapping, Optional, Tuple
 
-from whylogs.core.datatypes import DataType, TypeMapper
+from whylogs.core.datatypes import TypeMapper
 from whylogs.core.metrics.metrics import MetricConfig
 from whylogs.core.resolvers import STANDARD_RESOLVER, MetricSpec, ResolverSpec
 from whylogs.core.schema import DatasetSchema, DeclarativeSchema
@@ -19,73 +17,38 @@ logger = logging.getLogger(__name__)
 @dataclass
 class UdfSpec:
     """
-    Defines UDFs to apply to matching input columns. If column_name is a string,
-    the udfs will be applied to the named columns. The UDFs are passed the single
-    value in the column as input. If column_name is a list of strings, the UDF is
+    Defines UDFs to apply to matching input columns. Tthe UDF is
     passed a dictionary or dataframe with the named columns available (the UDF will
-    not be called unless all the named columns are available). If column_name is None,
-    input columns can be matched by column_type.
-
-    In the cases where there's a single input column (column_name is a stirng or
-    matching by column_type), the new column name is f"{input_column_name}.{udf_key}",
-    where udf_key is the key in the dictionary of UDFs. If column_name is a list of
-    stings (even of length 1), the new column name is just the udf_key.
+    not be called unless all the named columns are available). The new column name
+    the key in the udfs dictionary.
     """
 
-    column_name: Optional[Union[str, List[str]]] = None
-    column_type: Optional[DataType] = None
+    column_names: List[str]
     udfs: Dict[str, Callable[[Any], Any]] = field(
         default_factory=dict
     )  # new column name -> callable to compute new column value
 
     def __post_init__(self):
-        if isinstance(self.column_name, list) and (
-            len(self.column_name) == 0 or not all([isinstance(x, str) for x in self.column_name])
-        ):
-            raise ValueError("UdfSpec: column name must be either a single string or a non-empty list of strings")
-        if self.column_name and self.column_type:
-            logger.warning(f"UdfSpec: column(s) {self.column_name} also specified type, names takes precedence")
-        if not (self.column_name or self.column_type):
-            raise ValueError("UdfSpec: specification must supply name(s) or type")
-
-        if self.column_type and not issubclass(self.column_type, DataType):
-            raise ValueError("UdfSpec: column type must be a DataType")
+        if len(self.column_names) == 0 or not all([isinstance(x, str) for x in self.column_names]):
+            raise ValueError("UdfSpec: column_names must be either a non-empty list of strings")
 
 
-def _apply_udfs_on_row(value: Any, column: Optional[str], udfs: Dict, new_columns: Dict[str, Any]) -> None:
-    if column is None:  # multi-column UDF
-        for new_col, udf in udfs.items():
-            try:
-                new_columns[new_col] = udf(value)
-            except Exception:  # noqa
-                logger.exception(f"Evaluating multi-column UDF {new_col} failed")
-        return
-
+def _apply_udfs_on_row(value: Any, udfs: Dict, new_columns: Dict[str, Any]) -> None:
     for new_col, udf in udfs.items():
-        if new_col in new_columns:
-            logger.warning(f"UDF {udf.__name__} overwriting column {new_col}")
         try:
-            new_columns[f"{column}.{new_col}"] = udf(value)  # type: ignore
+            new_columns[new_col] = udf(value)
         except Exception:  # noqa
-            logger.exception(f"Evaluating UDF {new_col} failed on column {column}")
+            new_columns[new_col] = None
+            logger.exception(f"Evaluating multi-column UDF {new_col} failed")
 
 
-def _apply_udfs_on_dataframe(pandas: pd.DataFrame, column: Optional[str], udfs: Dict, new_df: pd.DataFrame) -> None:
-    if column is None:  # multi-column UDF
-        for new_col, udf in udfs.items():
-            try:
-                new_df[new_col] = udf(pandas)
-            except Exception:  # noqa
-                logger.exception(f"Evaluating UDF {new_col} failed on columns {pandas.keys()}")
-        return
-
+def _apply_udfs_on_dataframe(pandas: pd.DataFrame, udfs: Dict, new_df: pd.DataFrame) -> None:
     for new_col, udf in udfs.items():
-        if new_col in new_df.keys():
-            logger.warning(f"UDF {udf.__name__} overwriting column {new_col}")
         try:
-            new_df[f"{column}.{new_col}"] = pandas[column].map(udf)
+            new_df[new_col] = udf(pandas)
         except Exception:  # noqa
-            logger.exception(f"Evaluating UDF {new_col} failed on column {column}")
+            # new_df[new_col] = pandas[pandas.keys()[0]].apply(lambda x: None)
+            logger.exception(f"Evaluating UDF {new_col} failed on columns {pandas.keys()}")
 
 
 class UdfSchema(DeclarativeSchema):
@@ -116,47 +79,23 @@ class UdfSchema(DeclarativeSchema):
             segments=segments,
             validators=validators,
         )
-        self.name_udfs: Dict[str, Dict[str, Callable]] = defaultdict(dict)
-        self.type_udfs: Dict[str, Dict[str, Callable]] = defaultdict(dict)
-        self.multicolumn_udfs: List[UdfSpec] = []
         udf_specs = udf_specs if udf_specs else []
-        for spec in udf_specs:
-            if isinstance(spec.column_name, list):
-                self.multicolumn_udfs.append(spec)
-            elif spec.column_name:
-                self.name_udfs[spec.column_name].update(spec.udfs)
-            else:
-                self.type_udfs[spec.column_type.__name__].update(spec.udfs)  # type: ignore
+        self.multicolumn_udfs = list(udf_specs)
 
     def copy(self) -> DatasetSchema:
         copy = super().copy()
-        copy.name_udf_specs = self.name_udfs.copy()
-        copy.type_udf_specs = self.name_udfs.copy()
+        copy.multicolumn_udfs = list(self.multicolumn_udfs)
         return copy
 
     def _run_udfs_on_row(self, row: Mapping[str, Any], new_columns: Dict[str, Any]) -> None:
         for spec in self.multicolumn_udfs:
-            if spec.column_name and set(spec.column_name).issubset(set(row.keys())):
-                _apply_udfs_on_dataframe(row, None, spec.udfs, new_columns)
-
-        for column, value in row.items():
-            why_type = type(self.type_mapper(type(value))).__name__
-            if column in self.name_udfs:
-                _apply_udfs_on_row(value, column, self.name_udfs[column], new_columns)
-            elif why_type in self.type_udfs:
-                _apply_udfs_on_row(value, column, self.type_udfs[why_type], new_columns)
+            if set(spec.column_names).issubset(set(row.keys())):
+                _apply_udfs_on_dataframe(row, spec.udfs, new_columns)
 
     def _run_udfs_on_dataframe(self, pandas: pd.DataFrame, new_df: pd.DataFrame) -> None:
         for spec in self.multicolumn_udfs:
-            if spec.column_name and set(spec.column_name).issubset(set(pandas.keys())):
-                _apply_udfs_on_dataframe(pandas[spec.column_name], None, spec.udfs, new_df)
-
-        for column in pandas.keys():
-            why_type = type(self.type_mapper(pandas.dtypes[column])).__name__
-            if column in self.name_udfs:
-                _apply_udfs_on_dataframe(pandas, column, self.name_udfs[column], new_df)
-            elif why_type in self.type_udfs:
-                _apply_udfs_on_dataframe(pandas, column, self.type_udfs[why_type], new_df)
+            if set(spec.column_names).issubset(set(pandas.keys())):
+                _apply_udfs_on_dataframe(pandas[spec.column_names], spec.udfs, new_df)
 
     def _run_udfs(
         self, pandas: Optional[pd.DataFrame] = None, row: Optional[Dict[str, Any]] = None
@@ -172,27 +111,20 @@ class UdfSchema(DeclarativeSchema):
         return new_df, new_columns
 
 
-_col_name_udfs: Dict[str, Dict[str, Callable[[Any], Any]]] = defaultdict(dict)
-_col_type_udfs: Dict[str, Dict[str, Callable[[Any], Any]]] = defaultdict(dict)
 _multicolumn_udfs: List[UdfSpec] = []
 _resolver_specs: List[ResolverSpec] = []
 
 
 def register_dataset_udf(
-    col_name: Optional[Union[str, List[str]]] = None,
-    col_type: Optional[DataType] = None,
+    col_names: List[str],
     udf_name: Optional[str] = None,
     metrics: Optional[List[MetricSpec]] = None,
 ) -> Callable[[Any], Any]:
     """
     Decorator to easily configure UDFs for your data set. Decorate your UDF
     functions, then call generate_udf_dataset_schema() to create a UdfSchema
-    that includes the UDFs configured by your decorator parameters.
-
-    You must specify exactly one of either col_name or col_type. col_name will attach
-    a UDF to the named input column(s). col_type will attach a UDF to all
-    input columns of the specified type. The decorated function will automatically
-    be a UDF in the UdfSchema.
+    that includes the UDFs configured by your decorator parameters. The decorated
+    function will automatically be a UDF in the UdfSchema.
 
     Specify udf_name to give the output of the UDF a name. udf_name
     defautls to the name of the decorated function. Note that all lambdas are
@@ -201,28 +133,11 @@ def register_dataset_udf(
     """
 
     def decorator_register(func):
-        global _col_name_udfs, _col_type_udfs, _metric_specs, _multicolumn_udfs
-
-        if col_name is not None and col_type is not None:
-            raise ValueError("Only specify one of column name or type")
-
-        if col_name is None and col_type is None:
-            raise ValueError("Must specify one of column name or type")
-
-        if metrics and col_type:
-            raise ValueError("You cannot specify both metrics and column type")
-
+        global _multicolumn_udfs, _resolver_specs
         name = udf_name or func.__name__
-        if isinstance(col_name, list):
-            _multicolumn_udfs.append(UdfSpec(col_name, None, {name: func}))
-        elif col_name is not None:
-            _col_name_udfs[col_name][name] = func
-        else:
-            _col_type_udfs[col_type.__name__][name] = func
-
+        _multicolumn_udfs.append(UdfSpec(col_names, {name: func}))
         if metrics:
-            output_name = name if isinstance(col_name, list) else f"{col_name}.{name}"
-            _resolver_specs.append(ResolverSpec(output_name, None, deepcopy(metrics)))
+            _resolver_specs.append(ResolverSpec(name, None, deepcopy(metrics)))
 
         return func
 
@@ -238,31 +153,20 @@ def generate_udf_specs(other_udf_specs: Optional[List[UdfSpec]] = None) -> List[
 
     For example:
 
-    @register_dataset_udf(col_name="col1")
+    @register_dataset_udf(col_names=["col1"])
     def add5(x):
         return x + 5
-
-    @register_dataset_udf(col_type=String)
-    def upper(x):
-        return x.upper()
 
     schema = UdfSchema(STANDARD_RESOLVER, udf_specs=generate_udf_specs())
     why.log(data, schema=schema)
 
     This will attach a UDF to column "col1" that will generate a new column
-    named "col1.add5" containing the values in "col1" incremented by 5, and a UdfMetric
-    for each string column that will include a submetric named "<source>.upper" tracking
-    the uppercased strings in the input columns. Since these are appended to the
-    STANDARD_RESOLVER, the default metrics are also tracked for every column.
+    named "add5" containing the values in "col1" incremented by 5. Since these
+    are appended to the STANDARD_RESOLVER, the default metrics are also tracked
+    for every column.
     """
     specs = list(other_udf_specs) if other_udf_specs else []
     specs += _multicolumn_udfs
-    for col_name, udfs in _col_name_udfs.items():
-        specs.append(UdfSpec(col_name, None, udfs))
-
-    for col_type, udfs in _col_type_udfs.items():
-        specs.append(UdfSpec(None, getattr(sys.modules["whylogs.core.datatypes"], col_type), udfs))
-
     return specs
 
 
