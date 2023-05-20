f3126bfb820f7eb4b07e4006145746ba74858837
richard-rogers <93153899+richard-rogers@users.noreply.github.com>
2023-05-19 18:38:13 -0700

    Update python/whylogs/experimental/core/udf_schema.py

--- python/whylogs/experimental/core/udf_schema.py
+++ python/whylogs/experimental/core/udf_schema.py
@@ -131,6 +131,12 @@ def register_dataset_udf(
     defautls to the name of the decorated function. Note that all lambdas are
     named "lambda", so omitting udf_name on more than one lambda will result
     in name collisions.
+    
+    If any metrics are passed via the metrics argument, they will be attached
+    to the column produced by the UDF via the schema returned by generate_udf_dataset_schema().
+    If metrics is None, the UDF output column will get the metrics determined by
+    the other resolvers passed to generate_udf_dataset_schema(), or the STANDARD_RESOLVER
+    by default.
     """
 
     def decorator_register(func):
