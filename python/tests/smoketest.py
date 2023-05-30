b08a701ba894050aa5a2508b3268ff2b1b027a9b
Jamie Broomall <88007022+jamie256@users.noreply.github.com>
2023-05-30 17:03:27 -0500

    Bump version to release version 1.1.43 (#1260)
    Automated changes by
    [create-pull-request](https://github.com/peter-evans/create-pull-request)
    GitHub action
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- python/tests/smoketest.py
+++ python/tests/smoketest.py
@@ -12,7 +12,7 @@ intended to test the wheel in a production environment,
 not a development environment.
 """
 
-current_version = "1.1.42"
+current_version = "1.1.43"
 
 
 def test_package_version() -> None:
