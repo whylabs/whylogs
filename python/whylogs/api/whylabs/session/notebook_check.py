07bfbee0b46bad11ccce2700cbfa48e6b7ac4908
Jamie Broomall <jamie@whylabs.ai>
2023-06-07 20:45:22 -0700

    The colab check is not needed with updated check for ipykernel

--- python/whylogs/api/whylabs/session/notebook_check.py
+++ python/whylogs/api/whylabs/session/notebook_check.py
@@ -1,7 +1,7 @@
 def is_notebook() -> bool:
     result = False
     try:
-        result = is_ipython_notebook() or is_colab_notebook()
+        result = is_ipython_notebook()
     except Exception:
         pass
     return result
@@ -13,10 +13,4 @@ def is_ipython_notebook() -> bool:
     """
     import sys
 
-    return 'ipykernel' in sys.modules
-
-
-def is_colab_notebook() -> bool:
-    import importlib.util
-
-    return importlib.util.find_spec("google.colab") is not None
+    return "ipykernel" in sys.modules
