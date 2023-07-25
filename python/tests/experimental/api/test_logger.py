f640413895639e41e344a5b1504d9aeec2a739dd
Bernease Herman <bernease@gmail.com>
2023-07-25 12:59:36 -0700

    Add nDCG and more unit tests

--- python/tests/experimental/api/test_logger.py
+++ python/tests/experimental/api/test_logger.py
@@ -1,3 +1,4 @@
+from math import isclose
 from whylogs.core.stubs import pd
 from whylogs.experimental.api.logger import log_batch_ranking_metrics
 
@@ -15,29 +16,31 @@ def test_log_batch_ranking_metrics_single_simple():
         }
     )
     result = log_batch_ranking_metrics(
-        k=1, data=single_df, prediction_column="raw_predictions", target_column="raw_targets"
+        data=single_df, prediction_column="raw_predictions", target_column="raw_targets", convert_non_numeric=True
     )
     pandas_summary = result.view().to_pandas()
 
     k = 1
     column_names = [
-        "mean_average_precision_k_" + str(k),
-        "accuracy_k_" + str(k),
+        "mean_average_precision",
+        "accuracy",
         "mean_reciprocal_rank",
-        "precision_k_" + str(k),
-        "recall_k_" + str(k),
+        "precision",
+        "recall",
         "top_rank",
-        "average_precision_k_" + str(k),
+        "average_precision",
+        "norm_dis_cumul_gain"
     ]
     for col in column_names:
         assert col in pandas_summary.index
-    assert pandas_summary.loc["mean_average_precision_k_" + str(k), "counts/n"] == 1
-    assert pandas_summary.loc["accuracy_k_" + str(k), "counts/n"] == 1
+    assert pandas_summary.loc["mean_average_precision", "counts/n"] == 1
+    assert pandas_summary.loc["accuracy", "counts/n"] == 1
     assert pandas_summary.loc["mean_reciprocal_rank", "counts/n"] == 1
-    assert pandas_summary.loc["precision_k_" + str(k), "counts/n"] == 4
-    assert pandas_summary.loc["recall_k_" + str(k), "counts/n"] == 4
+    assert pandas_summary.loc["precision", "counts/n"] == 4
+    assert pandas_summary.loc["recall", "counts/n"] == 4
     assert pandas_summary.loc["top_rank", "counts/n"] == 4
-    assert pandas_summary.loc["average_precision_k_" + str(k), "counts/n"] == 4
+    assert pandas_summary.loc["average_precision", "counts/n"] == 4
+    assert pandas_summary.loc["norm_dis_cumul_gain", "counts/n"] == 1
 
 
 def test_log_batch_ranking_metrics_binary_simple():
@@ -45,7 +48,7 @@ def test_log_batch_ranking_metrics_binary_simple():
         {"raw_predictions": [[True, False, True], [False, False, False], [True, True, False], [False, True, False]]}
     )
 
-    result = log_batch_ranking_metrics(k=2, data=binary_df, prediction_column="raw_predictions")
+    result = log_batch_ranking_metrics(data=binary_df, prediction_column="raw_predictions", k=2, convert_non_numeric=True)
     pandas_summary = result.view().to_pandas()
 
     k = 2
@@ -57,6 +60,7 @@ def test_log_batch_ranking_metrics_binary_simple():
         "recall_k_" + str(k),
         "top_rank",
         "average_precision_k_" + str(k),
+        "norm_dis_cumul_gain_k_" + str(k)
     ]
     for col in column_names:
         assert col in pandas_summary.index
@@ -67,6 +71,7 @@ def test_log_batch_ranking_metrics_binary_simple():
     assert pandas_summary.loc["recall_k_" + str(k), "counts/n"] == 4
     assert pandas_summary.loc["top_rank", "counts/n"] == 4
     assert pandas_summary.loc["average_precision_k_" + str(k), "counts/n"] == 4
+    assert pandas_summary.loc["norm_dis_cumul_gain_k_" + str(k), "counts/n"] == 1
 
 
 def test_log_batch_ranking_metrics_multiple_simple():
@@ -83,7 +88,7 @@ def test_log_batch_ranking_metrics_multiple_simple():
     )
 
     result = log_batch_ranking_metrics(
-        k=3, data=multiple_df, prediction_column="raw_predictions", target_column="raw_targets"
+        data=multiple_df, prediction_column="raw_predictions", target_column="raw_targets", k=3, convert_non_numeric=True
     )
     pandas_summary = result.view().to_pandas()
 
@@ -96,6 +101,7 @@ def test_log_batch_ranking_metrics_multiple_simple():
         "recall_k_" + str(k),
         "top_rank",
         "average_precision_k_" + str(k),
+        "norm_dis_cumul_gain_k_" + str(k)
     ]
     for col in column_names:
         assert col in pandas_summary.index
@@ -106,3 +112,6 @@ def test_log_batch_ranking_metrics_multiple_simple():
     assert pandas_summary.loc["recall_k_" + str(k), "counts/n"] == 4
     assert pandas_summary.loc["top_rank", "counts/n"] == 4
     assert pandas_summary.loc["average_precision_k_" + str(k), "counts/n"] == 4
+    assert pandas_summary.loc["norm_discounted_cumul_gain_k_" + str(k), "counts/n"] == 1
+
+    assert isclose(pandas_summary.loc["norm_dis_cumul_gain_k_4", "distribution/median"], 0.35202, abs_tol=0.00001)
--- python/whylogs/experimental/api/logger/__init__.py
+++ python/whylogs/experimental/api/logger/__init__.py
@@ -4,27 +4,57 @@ from typing import Optional, Union
 from whylogs.api.logger import log
 from whylogs.api.logger.result_set import ProfileResultSet
 from whylogs.core import DatasetSchema
-from whylogs.core.stubs import np, pd
+from whylogs.core.stubs import np, pd, is_not_stub
 
 diagnostic_logger = logging.getLogger(__name__)
 
 
 def log_batch_ranking_metrics(
-    k: int,
     data: pd.core.frame.DataFrame,
     prediction_column: str,
     target_column: Optional[str] = None,
     score_column: Optional[str] = None,
+    k: int = None,
+    convert_non_numeric = False,
     schema: Union[DatasetSchema, None] = None,
     log_full_data: bool = False,
 ) -> ProfileResultSet:
+    """
+    Function to track ranking-related metrics based on validation data.
+    User may also pass the associated attribute names associated with
+    target, prediction, and/or score. Ignores ties in prediction score.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        pandas dataframe holding relevant data for logging
+    prediction_column : str
+        column name of that holding prediction values
+    target_column : str
+        column name of that holding target values / actuals
+    score_column : str
+        ignored, kept for API similarity
+    k : int (defaults to None)
+        number of query results to account into metrics, will alter 
+        output feature names
+    convert_non_numeric : bool (defaults to False)
+        converts non-numeric data to based on target values, assuming data
+        is sorted from most to least relevant in each row
+    schema : DatasetSchema
+        dataset schema can be passed for logging
+    log_full_data : bool
+        if true, will log raw data in addition to ranking metrics
+    """
+
+    if not is_not_stub(np):
+        raise ValueError("This experimental ranking API requires numpy import.")
+    
     formatted_data = data.copy(deep=True)  # TODO: does this have to be deep?
 
     relevant_cols = [prediction_column]
     if target_column is None:
         target_column = "__targets"
-        formatted_data[target_column] = True
-        formatted_data[target_column].apply(lambda x: [x])
+        formatted_data[target_column] = formatted_data[prediction_column].apply(lambda x: list(range(len(x)))[::-1])
     relevant_cols.append(target_column)
     if score_column is not None:
         relevant_cols.append(score_column)
@@ -34,6 +64,8 @@ def log_batch_ranking_metrics(
             # wrapping in lists because at least one isn't a list
             # TODO: more error checking
             formatted_data[col] = formatted_data[col].apply(lambda x: [x])
+    
+    _max_k = formatted_data[prediction_column].apply(len).max()
 
     formatted_data["count_at_k"] = formatted_data[relevant_cols].apply(
         lambda row: sum([1 if pred_val in row[target_column] else 0 for pred_val in row[prediction_column][:k]]), axis=1
@@ -52,13 +84,13 @@ def log_batch_ranking_metrics(
 
     formatted_data["top_rank"] = formatted_data[relevant_cols].apply(get_top_rank, axis=1)
 
-    output_data = (formatted_data["count_at_k"] / k).to_frame()
-    output_data.columns = ["precision_k_" + str(k)]
-    output_data["recall_k_" + str(k)] = formatted_data["count_at_k"] / formatted_data["count_all"]
+    output_data = (formatted_data["count_at_k"] / (k if k else 1)).to_frame()
+    output_data.columns = ["precision" + ("_k_" + str(k) if k else "")]
+    output_data["recall" + ("_k_" + str(k) if k else "")] = formatted_data["count_at_k"] / formatted_data["count_all"]
     output_data["top_rank"] = formatted_data["top_rank"]
 
     ki_dict: pd.DataFrame = None
-    for ki in range(1, k + 1):
+    for ki in range(1, (k if k else _max_k) + 1):
         ki_result = (
             formatted_data[relevant_cols].apply(
                 lambda row: sum(
@@ -74,18 +106,42 @@ def log_batch_ranking_metrics(
         else:
             ki_dict["p@" + str(ki)] = ki_result
 
-    output_data["average_precision_k_" + str(k)] = ki_dict.mean(axis=1)
-    mAP_at_k = output_data["average_precision_k_" + str(k)].mean(axis=0)
+    output_data["average_precision" + ("_k_" + str(k) if k else "")] = ki_dict.mean(axis=1)
+
+    def _convert_non_numeric(row_dict):
+        print("Converting non-numeric")
+        return (
+            [row_dict[target_column].index(pred_val) if pred_val in row_dict[target_column] else -1 for pred_val in row_dict[prediction_column]],
+            list(range(len(row_dict[prediction_column])))[::-1]
+        )
+
+    if convert_non_numeric:
+        formatted_data[[prediction_column, target_column]] = formatted_data.apply(_convert_non_numeric,
+                                                                                                result_type='expand', 
+                                                                                                axis=1)
+
+    def _calculate_row_ndcg(row_dict, k):
+        predicted_order = np.array(row_dict[prediction_column]).argsort()[::-1]
+        target_order = np.array(row_dict[target_column]).argsort()[::-1]
+        dcg_vals = [(rel / math_log(i + 2, 2)) for i, rel in enumerate(np.array(row_dict[target_column])[predicted_order][:k])]
+        idcg_vals = [(rel / math_log(i + 2, 2)) for i, rel in enumerate(np.array(row_dict[target_column])[target_order][:k])]
+        return sum(dcg_vals)/sum(idcg_vals)
+    
+    formatted_data["norm_dis_cumul_gain_k_" + str(k)] = formatted_data.apply(_calculate_row_ndcg, args=(k,), axis=1)
+
+    mAP_at_k = ki_dict.mean()
     hit_ratio = formatted_data["count_at_k"].apply(lambda x: bool(x)).sum() / len(formatted_data)
-    mrr = (1 / output_data["top_rank"]).replace([np.inf], np.nan).mean()
+    mrr = (1 / formatted_data["top_rank"]).replace([np.inf], np.nan).mean()
+    ndcg = formatted_data["norm_dis_cumul_gain_k_" + str(k)].mean()
 
     result = log(pandas=output_data, schema=schema)
     result = result.merge(
         log(
             row={
-                "mean_average_precision_k_" + str(k): mAP_at_k,
-                "accuracy_k_" + str(k): hit_ratio,
+                "mean_average_precision" + ("_k_" + str(k) if k else ""): mAP_at_k,
+                "accuracy" + ("_k_" + str(k) if k else ""): hit_ratio,
                 "mean_reciprocal_rank": mrr,
+                "norm_dis_cumul_gain" + ("_k_" + str(k) if k else ""): ndcg,
             },
             schema=schema,
         )
