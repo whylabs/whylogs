77f145d66c930e435d9089c0806099a077404ef2
Jamie Broomall <88007022+jamie256@users.noreply.github.com>
2023-10-03 11:48:43 -0500

    Update python/whylogs/api/whylabs/session/session_manager.py
    Co-authored-by: richard-rogers <93153899+richard-rogers@users.noreply.github.com>

--- python/whylogs/api/whylabs/session/session_manager.py
+++ python/whylogs/api/whylabs/session/session_manager.py
@@ -156,7 +156,7 @@ def _default_init() -> Session:
         il.warning_once("Initializing default session because no session was found.", logger.warning)
 
         # TODO: s/or local// ?
-        # To be safe, don't allow default session to be anonymous or local if this is happening as a side effect
+        # To be safe, don't allow default session to be anonymous if this is happening as a side effect
         # that users don't know about.
         return init(allow_anonymous=False, allow_local=True, force_local=True)
     else:
