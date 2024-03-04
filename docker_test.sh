af6eb1bb27459d596acb569b255f14a60bf2f7dc
richard-rogers <93153899+richard-rogers@users.noreply.github.com>
2024-03-04 16:37:48 +0000

    remove debugging cruft

--- docker_test.sh
+++ docker_test.sh
@@ -3,13 +3,5 @@
 git config --global --add safe.directory /workspace
 export PATH=$PATH:/home/whyuser/.local/bin
 cd /workspace/python
-whoami
-id
-mount
-df .
-ls -alh
-echo $PATH
-which poetry
-touch foo
 make install
 make test
