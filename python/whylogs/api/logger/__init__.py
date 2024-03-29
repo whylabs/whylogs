544d35db4b2b70bd4dc8780f5042b1e6d24fd1a5
Richard Rogers <richard@whylabs.ai>
2024-03-29 14:22:28 +0000

    notebook fixes

--- python/whylogs/api/logger/__init__.py
+++ python/whylogs/api/logger/__init__.py
@@ -321,7 +321,10 @@ def reader(name: str) -> ResultSetReader:
 
 
 def write(profile: DatasetProfile, base_dir: Optional[str] = None, filename: Optional[str] = None) -> None:
-    return profile.write(base_dir, filename)
+    if base_dir and filename:
+        return profile.write(base_dir, filename)
+    else:
+        return profile.write("", base_dir)  # backward compatibility: use base_dir as full filename
 
 
 def logger(
