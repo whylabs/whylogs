153e4f7381b7bee0380e89245afef95f7ba8ef56
Richard Rogers <richard@whylabs.ai>
2024-04-27 02:00:21 +0100

    version bump

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -8,7 +8,7 @@ if shutil.which("pandoc") is None:
     print("Pandoc is required to build our documentation.")
     sys.exit(1)
 
-version = "1.3.30"
+version = "1.4.0-rc0"
 
 project = "whylogs"
 author = "whylogs developers"
