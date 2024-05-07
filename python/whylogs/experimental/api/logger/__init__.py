d19272bbbb2f11850f83f3f82c5105cf11bdee2f
Jamie Broomall <88007022+jamie256@users.noreply.github.com>
2024-05-07 13:35:46 -0700

    drop extra columns in favor of recopying the dataframe
    Co-authored-by: richard-rogers <93153899+richard-rogers@users.noreply.github.com>

--- python/whylogs/experimental/api/logger/__init__.py
+++ python/whylogs/experimental/api/logger/__init__.py
@@ -354,7 +354,10 @@ def log_batch_ranking_metrics(
     output_data["reciprocal_rank"] = mrr
 
     if schema and schema.segments:
-        formatted_data = data.copy(deep=True)  # TODO: does this have to be deep?
+        for column in set(formatted_data.columns):
+            if column not in original_columns:
+                formatted_data = formatted_data.drop(column, axis=1)
+
         if log_full_data:
             return log(pandas=pd.concat([formatted_data, output_data], axis=1), schema=schema)
         else:
