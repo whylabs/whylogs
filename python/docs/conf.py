a31efd144958005d18b22e475ce2c6f76b5d9ebf
Jamie Broomall <88007022+jamie256@users.noreply.github.com>
2024-05-08 15:31:35 -0500

    Bump version to release version 1.3.32 (#1516)
    Automated changes by
    [create-pull-request](https://github.com/peter-evans/create-pull-request)
    GitHub action
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -8,7 +8,7 @@ if shutil.which("pandoc") is None:
     print("Pandoc is required to build our documentation.")
     sys.exit(1)
 
-version = "1.3.31"
+version = "1.3.32"
 
 project = "whylogs"
 author = "whylogs developers"
