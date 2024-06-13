ce9d04bb67ddae5a9f4f8bfe1c965cc1f8e72ece
Richard Rogers <richard@whylabs.ai>
2024-06-13 13:02:45 -0700

    pre-commit

--- python/tests/api/writer/test_writables.py
+++ python/tests/api/writer/test_writables.py
@@ -1,12 +1,10 @@
 import os
+
 import pandas as pd
 
 import whylogs as why
-
-from whylogs.core.dataset_profile import DatasetProfile
 from whylogs.core.schema import DatasetSchema
 from whylogs.core.segmentation_partition import segment_on_column
-from whylogs.core.view.dataset_profile_view import DatasetProfileView
 from whylogs.core.view.segmented_dataset_profile_view import SegmentedDatasetProfileView
 from whylogs.viz.extensions.reports.profile_summary import ProfileSummaryReport
 
@@ -46,7 +44,7 @@ def test_html_report_write(tmp_path):
     view = why.log({"a": 1}).profile().view()
     report = ProfileSummaryReport(view)
     path = os.path.join(tmp_path, "test_html_report_write.bin")
-    success, files = view.write(path)
+    success, files = report.write(path)
     assert success
     assert path in files
     assert os.path.isfile(path)
