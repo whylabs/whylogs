96b64aa649ce8baf83d37a3338ceda5d8fe8b000
richard-rogers <93153899+richard-rogers@users.noreply.github.com>
2024-06-27 03:03:18 +0100

    Update docker_test.sh

--- docker_test.sh
+++ docker_test.sh
@@ -3,7 +3,7 @@
 git config --global --add safe.directory /workspace
 export PATH=$PATH:/home/whyuser/.local/bin
 WHYLABS_DEFAULT_DATASET_ID=model-2250
-WHYLABS_DEFAULT_ORG_ID=org-0
+'```
 WHYLABS_NO_ANALYTICS=True
 WHYLABS_API_KEY=${{ secrets.WL_API_KEY }}
 WHYLABS_API_ENDPOINT=${{ secrets.WL_END_POINT }}
