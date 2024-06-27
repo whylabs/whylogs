f7643ec343c3a3a4624891dde59e07bcc3643247
richard-rogers <93153899+richard-rogers@users.noreply.github.com>
2024-06-27 15:13:31 -0700

    Add integration tests to nightly test suite (#1535)
    ## Description
    
    Includes integration tests in nightly test run.
    
    
    - [ ] I have reviewed the [Guidelines for Contributing](CONTRIBUTING.md)
    and the [Code of Conduct](CODE_OF_CONDUCT.md).

--- docker_test.sh
+++ docker_test.sh
@@ -2,6 +2,11 @@
 
 git config --global --add safe.directory /workspace
 export PATH=$PATH:/home/whyuser/.local/bin
+WHYLABS_DEFAULT_DATASET_ID=model-2250
+WHYLABS_NO_ANALYTICS=True
+WHYLABS_API_KEY=${{ secrets.WL_API_KEY }}
+WHYLABS_API_ENDPOINT=${{ secrets.WL_END_POINT }}
+
 cd /workspace/python
 make install
-make test pre-commit
+make test pre-commit && poetry run pytest tests/api/writer/test_whylabs_integration.py --load
