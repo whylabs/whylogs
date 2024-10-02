0a76ed2cc8d52e337cf829841609a551ff2ff30b
Jamie Broomall <88007022+jamie256@users.noreply.github.com>
2024-10-02 11:55:10 -0500

    Update version to 1.5.0 (#1572)
    ## Description
    * Update version to 1.5.0
    
    - [x] I have reviewed the [Guidelines for Contributing](CONTRIBUTING.md)
    and the [Code of Conduct](CODE_OF_CONDUCT.md).

--- python/tests/smoketest.py
+++ python/tests/smoketest.py
@@ -12,7 +12,7 @@ intended to test the wheel in a production environment,
 not a development environment.
 """
 
-current_version = "1.4.8"
+current_version = "1.5.0"
 
 
 def test_package_version() -> None:
