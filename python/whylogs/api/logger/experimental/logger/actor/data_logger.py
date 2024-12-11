6cf62bbc72c833fb032f609eb9d9df751cd5ae37
Richard Rogers <richard@whylabs.ai>
2024-12-11 02:46:37 +0000

    use pandas stubs

--- python/whylogs/api/logger/experimental/logger/actor/data_logger.py
+++ python/whylogs/api/logger/experimental/logger/actor/data_logger.py
@@ -1,11 +1,7 @@
 from abc import abstractmethod
 from typing import Any, Dict, Generic, List, Optional, TypeVar, Union
 
-# TODO: stubs?
-try:
-    import pandas as pd  # type: ignore
-except ImportError:
-    pd: Any = None  # type: ignore
+from whylogs.core.stubs import pd
 
 
 # TODO add strong typing here. whylogs takes pretty much anything in log() so we don't actually
